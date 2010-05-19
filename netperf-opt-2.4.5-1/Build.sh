08a09a48f54e6f2c8c55dd763b08f27204105bea
Jens Låås <jens@laas.mine.nu>
2010-05-19 12:13:34 +0200

    changed named to include opt

--- netperf-opt-2.4.5-1/Build.sh
+++ netperf-opt-2.4.5-1/Build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SRCVER=netperf-2.4.5
-PKG=$SRCVER-1 # with build version
+PKG=netperf-opt-2.4.5-1 # with build version
 
 # PKGDIR is set by 'pkg_build'. Usually "/var/lib/build/$PKG".
 PKGDIR=${PKGDIR:-/var/lib/build/$PKG}
