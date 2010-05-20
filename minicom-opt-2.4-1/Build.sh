ebdf592c02dcb67170400a005e2eca343840c00a
Jens Låås <jens@laas.mine.nu>
2010-05-20 16:05:52 +0200

    optional pkg for minicom

---
+++ minicom-opt-2.4-1/Build.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+SRCVER=minicom-2.4
+PKG=minicom-opt-2.4-1 # with build version
+
+# PKGDIR is set by 'pkg_build'. Usually "/var/lib/build/$PKG".
+PKGDIR=${PKGDIR:-/var/lib/build/$PKG}
+SRC=/var/spool/src/$SRCVER.tar.gz
+BUILDDIR=/var/tmp/src/$SRCVER
+DST="/var/tmp/install/$PKG"
+
+#########
+# Install dependencies:
+# pkg_available dependency1-1 dependency2-1
+# pkg_install dependency1-1 || exit 1
+pkg_install libiconv-1.13.1-1 || exit 1
+pkg_install ncurses-lib-5.7-1 || exit 1
+
+#########
+# Unpack sources into dir under /var/tmp/src
+./Fetch-source.sh || exit 1
+cd $(dirname $BUILDDIR); tar xf $SRC
+
+#########
+# Patch
+cd $BUILDDIR
+libtool_fix-1
+# patch -p1 < $PKGDIR/mypatch.pat
+
+#########
+# Configure
+$PKGDIR/B-configure-1 --prefix=/ --bindir=/opt/minicom/bin --enable-lock-dir=/tmp || exit 1
+
+#########
+# Post configure patch
+# patch -p0 < $PKGDIR/Makefile.pat
+
+#########
+# Compile
+make || exit 1
+
+#########
+# Install into dir under /var/tmp/install
+rm -rf "$DST"
+make install DESTDIR=$DST # --with-install-prefix may be an alternative
+
+#########
+# Check result
+cd $DST
+# [ -f usr/bin/myprog ] || exit 1
+(ldd opt/minicom/bin/minicom|grep -qs "not a dynamic executable") || exit 1
+
+#########
+# Clean up
+cd $DST
+rm -rf share
+strip opt/minicom/bin/*
+
+#########
+# Make package
+cd $DST
+tar czf /var/spool/pkg/$PKG.tar.gz .
+
+#########
+# Cleanup after a success
+cd /var/lib/build
+[ "$DEVEL" ] || rm -rf "$DST"
+[ "$DEVEL" ] || rm -rf "$BUILDDIR"
+pkg_uninstall
+exit 0
