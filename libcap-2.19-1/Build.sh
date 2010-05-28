8780ab5b39fdf04b6e6a66feab8fb5d4de570289
Jens Låås <jens@laas.mine.nu>
2010-05-28 13:49:31 +0200

    libcap

---
+++ libcap-2.19-1/Build.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+SRCVER=libcap-2.19
+PKG=$SRCVER-1 # with build version
+
+# PKGDIR is set by 'pkg_build'. Usually "/var/lib/build/$PKG".
+PKGDIR=${PKGDIR:-/var/lib/build/$PKG}
+SRC=/var/spool/src/$SRCVER.tar.bz2
+BUILDDIR=/var/tmp/src/$SRCVER
+DST="/var/tmp/install/$PKG"
+
+#########
+# Simple inplace edit with sed.
+# Usage: sedit 's/find/replace/g' Filename
+function sedit {
+    sed "$1" $2 > /tmp/sedit.$$
+    cp /tmp/sedit.$$ $2
+    rm /tmp/sedit.$$
+}
+
+#########
+# Install dependencies:
+# pkg_available dependency1-1 dependency2-1
+pkg_install perl-5.10.1-1 || exit 1
+
+#########
+# Unpack sources into dir under /var/tmp/src
+./Fetch-source.sh || exit 1
+cd $(dirname $BUILDDIR); tar xf $SRC
+
+#########
+# Patch
+cd $BUILDDIR
+libtool_fix-1
+# patch -p1 < $PKGDIR/mypatch.pat
+
+#########
+# Configure
+#B-configure-1 --prefix=/usr || exit 1
+sedit 's/-O2/-Os -march=i586/g' Make.Rules
+sedit 's/-fPIC//g' Make.Rules
+sedit 's/LIBATTR := yes/LIBATTR := no/g' Make.Rules
+
+#########
+# Post configure patch
+# patch -p0 < $PKGDIR/Makefile.pat
+
+#########
+# Compile
+cd libcap
+make libcap.a || exit 1
+
+#########
+# Install into dir under /var/tmp/install
+rm -rf "$DST"
+mkdir -p $DST/usr/include/sys
+mkdir -p $DST/usr/lib
+cp include/sys/capability.h $DST/usr/include/sys/capability.h || exit 1
+cp libcap.a $DST/usr/lib || exit 1
+
+#########
+# Check result
+cd $DST
+# [ -f usr/bin/myprog ] || exit 1
+# (ldd sbin/myprog|grep -qs "not a dynamic executable") || exit 1
+
+#########
+# Clean up
+cd $DST
+# rm -rf usr/share usr/man
+[ -d bin ] && strip bin/*
+[ -d usr/bin ] && strip usr/bin/*
+
+#########
+# Make package
+cd $DST
+tar czf /var/spool/pkg/$PKG.tar.gz .
+
+#########
+# Cleanup after a success
+cd /var/lib/build
+[ "$DEVEL" ] || rm -rf "$DST"
+[ "$DEVEL" ] || rm -rf "$BUILDDIR"
+pkg_uninstall
+exit 0
