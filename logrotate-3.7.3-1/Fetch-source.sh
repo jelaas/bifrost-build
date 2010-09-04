79f421528677c6cd0d6ecfda8ea7c2ccdf5f2c8e
Jens Låås <jens@laas.mine.nu>
2010-09-04 21:43:52 +0200

    logrotate

---
+++ logrotate-3.7.3-1/Fetch-source.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SRC=logrotate-3.7.3.tar.gz
+DST=/var/spool/src/$SRC
+
+[ -s "$DST" ] || wget -O $DST http://ibiblio.download.sourcemage.org/pub/linux/distributions/sourcemage/mirror/$SRC
