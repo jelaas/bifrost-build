54342f96327c08a8de94164e5c5846cf5aba1903
Jens Låås <jens@laas.mine.nu>
2010-10-30 15:14:13 +0200

    alt download

--- all/libpng-1.4.3-1/Fetch-source.sh
+++ all/libpng-1.4.3-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=libpng-1.4.3.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST ftp://ftp.simplesystems.org/pub/libpng/png/src/$SRC || wget -O $DST http://bifrost-repo/$SRC
+[ -s "$DST" ] || wget -O $DST ftp://ftp.simplesystems.org/pub/libpng/png/src/$SRC || wget -O $DST ftp://ftp.simplesystems.org/pub/libpng/png/src/history/$SRC
