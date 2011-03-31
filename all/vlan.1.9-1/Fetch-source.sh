615989b547d44b373130cd6202cf814e12e9750a
Jens Låås <jens@laas.mine.nu>
2011-03-31 08:47:25 +0200

    alternate source location

--- all/vlan.1.9-1/Fetch-source.sh
+++ all/vlan.1.9-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=vlan.1.9.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://www.candelatech.com/~greear/vlan/$SRC
+[ -s "$DST" ] || wget -O $DST --no-check-certificate  https://laas.mine.nu/srcrepo/$SRC || wget -O $DST http://www.candelatech.com/~greear/vlan/$SRC
