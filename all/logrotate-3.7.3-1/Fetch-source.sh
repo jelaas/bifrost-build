a9ac46717ee708c103b5bdde9ba676fb4226820a
Jens Låås <jens@laas.mine.nu>
2011-03-31 09:09:37 +0200

    alternate source location

--- all/logrotate-3.7.3-1/Fetch-source.sh
+++ all/logrotate-3.7.3-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=logrotate-3.7.3.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://ibiblio.download.sourcemage.org/pub/linux/distributions/sourcemage/mirror/$SRC
+[ -s "$DST" ] || wget -O $DST http://ibiblio.download.sourcemage.org/pub/linux/distributions/sourcemage/mirror/$SRC || wget -O $DST http://dbg.download.sourcemage.org/mirror/$SRC
