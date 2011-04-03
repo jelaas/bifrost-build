62282b4fbfc6ab373a3adffa8dcbd94b79584ebe
Jens Låås <jens@laas.mine.nu>
2011-04-03 13:02:00 +0200

    alternate source location

--- all/console-tools-lib-0.2.3-1/Fetch-source.sh
+++ all/console-tools-lib-0.2.3-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=console-tools-0.2.3.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST ftp://metalab.unc.edu/pub/Linux/system/keyboards/$SRC
+[ -s "$DST" ] || wget -O $DST http://www.ibiblio.org/pub/Linux/system/keyboards/$SRC || wget -O $DST ftp://metalab.unc.edu/pub/Linux/system/keyboards/$SRC
