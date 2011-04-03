3c3ff048cc0945be6616d5d132bf9d353664aca9
Jens Låås <jens@laas.mine.nu>
2011-04-03 12:59:44 +0200

    alternate source location

--- all/console-data-1999.08.29-1/Fetch-source.sh
+++ all/console-data-1999.08.29-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=console-data-1999.08.29.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST ftp://metalab.unc.edu/pub/Linux/system/keyboards/$SRC
+[ -s "$DST" ] || wget -O $DST http://www.ibiblio.org/pub/Linux/system/keyboards/$SRC || wget -O $DST ftp://metalab.unc.edu/pub/Linux/system/keyboards/$SRC
