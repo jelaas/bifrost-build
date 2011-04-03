d5c4481ecc32355d221cbc39e67fb71b6c2c2c58
Jens Låås <jens@laas.mine.nu>
2011-04-03 15:40:25 +0200

    alternate source location

--- all/lsof-4.84-1/Fetch-source.sh
+++ all/lsof-4.84-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=lsof_4.84.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST ftp://lsof.itap.purdue.edu/pub/tools/unix/lsof/$SRC
+[ -s "$DST" ] || wget -O $DST ftp://ftp.fu-berlin.de/pub/unix/tools/lsof/$SRC || wget -O $DST ftp://lsof.itap.purdue.edu/pub/tools/unix/lsof/$SRC
