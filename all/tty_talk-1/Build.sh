47834ed224fb28559ec11486039bdedf6fb06d3a
Jens Låås <jens@laas.mine.nu>
2011-04-04 17:52:38 +0200

    tty_talk

---
+++ all/tty_talk-1/Build.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+SRCVER=tty_talk
+PKG=$SRCVER-1 # with build version
+
+# PKGDIR is set by 'pkg_build'. Usually "/var/lib/build/all/$PKG".
+PKGDIR=${PKGDIR:-/var/lib/build/all/$PKG}
+SRC=/var/spool/src/$SRCVER.tar.gz
+BUILDDIR=/var/tmp/src/$SRCVER
+DST="/var/tmp/install/$PKG"
+
+#########
+# Simple inplace edit with sed.
+# Usage: sedit 's/find/replace/g' Filename
+function sedit {
+    sed "$1" $2 > /tmp/sedit.$$
+    cp /tmp/sedit.$$ $2
+    rm /tmp/sedit.$$
+}
+
+#########
+# Fetch sources
+./Fetch-source.sh || exit $?
+pkg_uninstall # Uninstall any dependencies used by Fetch-source.sh
+
+#########
+# Install dependencies:
+# pkg_available dependency1-1 dependency2-1
+# pkg_install dependency1-1 || exit 2
+
+#########
+# Unpack sources into dir under /var/tmp/src
+mkdir -p $BUILDDIR
+cp -f $PKGDIR/tty_talk.c $BUILDDIR
+
+#########
+# Patch
+cd $BUILDDIR || exit 1
+
+#########
+# Configure
+
+#########
+# Post configure patch
+# patch -p0 < $PKGDIR/Makefile.pat
+
+#########
+# Compile
+gcc -o tty_talk -Os -march=i586 -static tty_talk.c
+strip tty_talk
+
+#########
+# Install into dir under /var/tmp/install
+rm -rf "$DST"
+mkdir -p $DST/sbin
+cp tty_talk $DST/sbin
+
+#########
+# Check result
+cd $DST || exit 1
+# [ -f usr/bin/myprog ] || exit 1
+# (ldd sbin/myprog|grep -qs "not a dynamic executable") || exit 1
+
+#########
+# Clean up
+cd $DST || exit 1
+# rm -rf usr/share usr/man
+[ -d bin ] && strip bin/*
+[ -d usr/bin ] && strip usr/bin/*
+
+#########
+# Make package
+cd $DST || exit 1
+tar czf /var/spool/pkg/$PKG.tar.gz .
+
+#########
+# Cleanup after a success
+cd /var/lib/build
+[ "$DEVEL" ] || rm -rf "$DST"
+[ "$DEVEL" ] || rm -rf "$BUILDDIR"
+pkg_uninstall
+exit 0
