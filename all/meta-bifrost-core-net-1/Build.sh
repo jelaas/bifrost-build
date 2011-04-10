9dab9a5b9a4f1717b6d47c7d312355320624c77d
Jens Låås <jens@laas.mine.nu>
2011-04-10 22:52:08 +0200

    dhclient-script

--- all/meta-bifrost-core-net-1/Build.sh
+++ all/meta-bifrost-core-net-1/Build.sh
@@ -42,6 +42,7 @@ cd $DST
 untar bind-9.7.0-P2-1 ./usr/bin/host
 untar bridge-utils-1.4-1
 untar dhcp-4.1-ESV-R2-1
+untar dhclient-script-1
 untar dhcpcd-4.0.15-1
 untar dnsmasq-2.52-1
 untar ethtool-v2.6.35-2
