00d2fcdb85c1474deee48e3b9e9a9c741f48ef00
Jens Låås <jens@laas.mine.nu>
2011-04-18 09:25:26 +0200

    use fdisk expertmode to set partition start

---
+++ all/make_bifrost-2/Build.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+SRCVER=make_bifrost
+PKG=$SRCVER-1 # with build version
+
+# PKGDIR is set by 'pkg_build'. Usually "/var/lib/build/all/$PKG".
+PKGDIR=${PKGDIR:-/var/lib/build/all/$PKG}
+SRC=/var/spool/src/$SRCVER.tar.gz
+BUILDDIR=/var/tmp/src/$SRCVER
+DST="/var/tmp/install/$PKG"
+
+#########
+# Simple inplace edit with sed.
+# Usage: sedit 's/find/replace/g' Filename
+function sedit {
+    sed "$1" $2 > /tmp/sedit.$$
+    cp /tmp/sedit.$$ $2
+    rm /tmp/sedit.$$
+}
+
+#########
+# Fetch sources
+./Fetch-source.sh || exit 1
+pkg_uninstall # Uninstall any dependencies used by Fetch-source.sh
+
+#########
+# Install dependencies:
+# pkg_available dependency1-1 dependency2-1
+# pkg_install dependency1-1 || exit 1
+
+#########
+# Install into dir under /var/tmp/install
+rm -rf "$DST"
+mkdir -p "$DST" || exit 1
+mkdir -p $DST/boot
+mkdir -p $DST/
+mkdir -p $DST/sbin
+mkdir -p $DST/Documentation
+
+cp "$PKGDIR/boot_image.gz" $DST/boot
+cp "$PKGDIR/make_bifrost-minihowto" $DST/Documentation
+cp "$PKGDIR/make_bifrost" $DST/sbin
+
+#########
+# Check result
+cd $DST || exit 1
+# [ -f usr/bin/myprog ] || exit 1
+# (ldd sbin/myprog|grep -qs "not a dynamic executable") || exit 1
+
+#########
+# Clean up
+cd $DST || exit 1
+# rm -rf usr/share usr/man
+
+#########
+# Make package
+cd $DST || exit 1
+tar czf /var/spool/pkg/$PKG.tar.gz .
+
+#########
+# Cleanup after a success
+cd /var/lib/build
+[ "$DEVEL" ] || rm -rf "$DST"
+[ "$DEVEL" ] || rm -rf "$BUILDDIR"
+pkg_uninstall
+exit 0
