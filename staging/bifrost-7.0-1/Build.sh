24de2210e78f327c1f8ff317045800a23d63e19e
Jens Låås <jens@laas.mine.nu>
2011-05-14 16:03:24 +0200

    2 more small patches to kernel

--- staging/bifrost-7.0-1/Build.sh
+++ staging/bifrost-7.0-1/Build.sh
@@ -176,8 +176,8 @@ untar wget-1.12-1
 
 # core
 
-untar kernel-x86_32-2.6.38-rc8-3
-untar kernel-x86_64-2.6.38-rc8-3
+untar kernel-x86_32-2.6.38-rc8-4
+untar kernel-x86_64-2.6.38-rc8-4
 
 untar bash-4.1-1
 untar binutils-2.20.1-1 ./usr/bin/size
