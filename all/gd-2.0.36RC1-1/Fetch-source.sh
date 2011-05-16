cfb40ce89a08275ddbb9083d7b4213580f4a62d1
Jens Låås <jens@laas.mine.nu>
2011-05-16 12:51:42 +0200

    add a source mirror

--- all/gd-2.0.36RC1-1/Fetch-source.sh
+++ all/gd-2.0.36RC1-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=gd-2.0.36RC1.tar.bz2 
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://www.libgd.org/releases/$SRC || wget -O $DST http://bifrost-repo/$SRC
+[ -s "$DST" ] || wget -O $DST http://www.libgd.org/releases/$SRC || wget -O $DST http://ring.nict.go.jp/archives/graphics/gd/$SRC
