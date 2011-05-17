d6b826a627c2b6cde8d38d23143e632410765f82
Jens Låås <jens@laas.mine.nu>
2011-05-17 15:19:03 +0200

    bufsize option

---
+++ all/apache-netlog-1.4-1/Fetch-source.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+V=1.4
+VER=apache-netlog-$V
+SRC=apache-netlog-$V.tar.gz
+DST=/var/spool/src/$SRC
+
+if [ ! -s "$DST" ]; then
+    pkg_install passwd-file-1 || exit 2
+    pkg_install git-1.7.1-2 || exit 2
+    pkg_install openssh-5.5p1-1 || exit 2
+    cd /tmp
+    rm -rf $VER
+    /opt/git/bin/git clone git://github.com/jelaas/apache-netlog.git $VER || exit 1
+    cd $VER
+    /opt/git/bin/git checkout $V || exit 1
+    cd /tmp
+    tar czf $DST $VER
+    rm -rf $VER
+fi
+
