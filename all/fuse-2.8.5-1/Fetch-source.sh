2a84ade74c0378319735f50d1141d99d5d6d10a9
Jens Låås <jens@laas.mine.nu>
2011-05-17 13:50:42 +0200

    add a mirror for source

--- all/fuse-2.8.5-1/Fetch-source.sh
+++ all/fuse-2.8.5-1/Fetch-source.sh
@@ -3,5 +3,4 @@
 SRC=fuse-2.8.5.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://ignum2.dl.sourceforge.net/project/fuse/fuse-2.X/2.8.5/fuse-2.8.5.tar.gz
-
+[ -s "$DST" ] || wget -O $DST http://ignum2.dl.sourceforge.net/project/fuse/fuse-2.X/2.8.5/fuse-2.8.5.tar.gz || wget -O $DST http://garr.dl.sourceforge.net/project/fuse/fuse-2.X/2.8.5/$SRC
