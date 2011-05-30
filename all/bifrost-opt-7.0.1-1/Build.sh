872eb07eedc1b29e0b8401d2982abca8e25abf63
Jens Låås <jens@laas.mine.nu>
2011-05-30 13:21:05 +0200

    +gpsd apache version

--- all/bifrost-opt-7.0.1-1/Build.sh
+++ all/bifrost-opt-7.0.1-1/Build.sh
@@ -20,11 +20,12 @@ function sedit {
 
 pkg_uninstall # Uninstall any dependencies used by Fetch-source.sh
 
-pkg_install opt-apache-2.2.17-1 || exit 2
+pkg_install opt-apache-2.2.19-1 || exit 2
 pkg_install opt-dovecot-2.0.12-2 || exit 2
 pkg_install opt-exim-4.72-1 || exit 2
 pkg_install opt-emacs-23.2-1 || exit 2
 pkg_install opt-gnuplot-4.4.0-1 || exit 2
+pkg_install opt-gpsd-2.95-1 || exit 2
 pkg_install opt-mfs-1.6.20-1 || exit 2
 pkg_install opt-minicom-2.4-2 || exit 2
 pkg_install opt-netperf-2.4.5-1 || exit 2
@@ -32,6 +33,5 @@ pkg_install opt-net-snmp-5.5-1 || exit 2
 pkg_install opt-quagga-0.99.17-2 || exit 2
 pkg_install opt-squid-3.1.7-1 || exit 2
 pkg_install opt-wpa_supplicant-0.7.3-1 || exit 2
-
 
 exit 0
