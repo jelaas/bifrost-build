ef00e3e4819302551a4c5beac78a7667103096b1
Jens Låås <jens@laas.mine.nu>
2011-06-01 13:54:39 +0200

    stty devname patch

---
+++ all/coreutils-7.6-2/Build.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+SRCVER=coreutils-7.6
+PKG=$SRCVER-2 # with build version
+
+# PKGDIR is set by 'pkg_build'. Usually "/var/lib/build/all/$PKG".
+PKGDIR=${PKGDIR:-/var/lib/build/all/$PKG}
+SRC=/var/spool/src/$SRCVER.tar.gz
+BUILDDIR=/var/tmp/src/$SRCVER
+DST="/var/tmp/install/$PKG"
+
+#########
+# Simple inplace edit with sed.
+# Usage: sedit 's/find/replace/g' Filename
+function sedit {
+    sed $1 $2 > /tmp/sedit.$$
+    cp /tmp/sedit.$$ $2
+    rm /tmp/sedit.$$
+}
+
+#########
+# Install dependencies:
+# pkg_available dependency1-1 dependency2-1
+# pkg_install dependency1-1 || exit 2
+pkg_install devname-1.0.3-1 || exit 2
+
+#########
+# Unpack sources into dir under /var/tmp/src
+./Fetch-source.sh || exit 1
+cd $(dirname $BUILDDIR); tar xf $SRC
+
+#########
+# Patch
+cd $BUILDDIR
+libtool_fix-1
+# patch -p1 < $PKGDIR/mypatch.pat
+echo "#define _XOPEN_SOURCE 600" > /tmp/dd.$$
+echo "#include <fcntl.h>" >> /tmp/dd.$$
+cat src/dd.c >> /tmp/dd.$$
+cp /tmp/dd.$$ src/dd.c; rm -f /tmp/dd.$$
+patch -p0 < $PKGDIR/stty_devname.pat || exit 1
+
+#########
+# Configure
+LIBS="-ldevname" $PKGDIR/B-configure-1 --prefix=/usr --bindir=/bin --disable-acl --disable-assert || exit 1
+
+#########
+# Post configure patch
+# patch -p0 < $PKGDIR/Makefile.pat
+
+#########
+# Compile
+make || exit 1
+
+#########
+# Install into dir under /var/tmp/install
+rm -rf "$DST"
+make install DESTDIR=$DST # --with-install-prefix may be an alternative
+
+#########
+# Check result
+cd $DST
+# [ -f usr/bin/myprog ] || exit 1
+(ldd bin/ls|grep -qs "not a dynamic executable") || exit 1
+
+#########
+# Clean up
+cd $DST
+rm -rf usr/share usr
+[ -d bin ] && strip bin/*
+
+#########
+# Make package
+cd $DST
+tar czf /var/spool/pkg/$PKG.tar.gz .
+
+#########
+# Cleanup after a success
+cd /var/lib/build
+[ "$DEVEL" ] || rm -rf "$DST"
+[ "$DEVEL" ] || rm -rf "$BUILDDIR"
+pkg_uninstall
+exit 0
