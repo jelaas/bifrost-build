ab2ba705f41f65718bbbb637c136cdda850aa540
Jens Låås <jens@laas.mine.nu>
2011-08-17 13:35:37 +0200

    new kernel

--- staging/bifrost-7.0-1/Build.sh
+++ staging/bifrost-7.0-1/Build.sh
@@ -179,8 +179,8 @@ untar wget-1.12-1
 
 # core
 
-untar kernel-x86_32-2.6.38-rc8-9
-untar kernel-x86_64-2.6.38-rc8-9
+untar kernel-x86_32-3.0.0-2
+untar kernel-x86_64-3.0.0-2
 
 untar bash-4.1-1
 untar binutils-2.20.1-1 ./usr/bin/size
