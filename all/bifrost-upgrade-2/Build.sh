16080b073db0d5c2a590539b0fc9565f070d0098
Jens Låås <jens@laas.mine.nu>
2011-08-17 14:14:42 +0200

    upgrade now handles /bin32 and /bin64

---
+++ all/bifrost-upgrade-2/Build.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+SRCVER=bifrost-upgrade
+PKG=$SRCVER-2 # with build version
+
+# PKGDIR is set by 'pkg_build'. Usually "/var/lib/build/all/$PKG".
+PKGDIR=${PKGDIR:-/var/lib/build/all/$PKG}
+SRC=/var/spool/src/$SRCVER.tar.gz
+BUILDDIR=/var/tmp/src/$SRCVER
+DST="/var/tmp/install/$PKG"
+
+#########
+# Simple inplace edit with sed.
+# Usage: sedit 's/find/replace/g' Filename
+function sedit {
+    sed "$1" $2 > /tmp/sedit.$$
+    cp /tmp/sedit.$$ $2
+    rm /tmp/sedit.$$
+}
+
+#########
+# Fetch sources
+pkg_uninstall # Uninstall any dependencies used by Fetch-source.sh
+
+#########
+# Install dependencies:
+# pkg_available dependency1-1 dependency2-1
+# pkg_install dependency1-1 || exit 2
+
+# Install into dir under /var/tmp/install
+rm -rf "$DST"
+mkdir -p "$DST/usr/sbin" || exit 1
+cp $PKGDIR/upgrade $DST/usr/sbin/bifrost-upgrade
+chmod +x $DST/usr/sbin/bifrost-upgrade
+
+#########
+# Check result
+cd $DST || exit 1
+# [ -f usr/bin/myprog ] || exit 1
+# (ldd sbin/myprog|grep -qs "not a dynamic executable") || exit 1
+
+#########
+# Clean up
+cd $DST || exit 1
+
+#########
+# Make package
+cd $DST || exit 1
+tar czf /var/spool/pkg/$PKG.tar.gz .
+
+#########
+# Cleanup after a success
+cd /var/lib/build
+[ "$DEVEL" ] || rm -rf "$DST"
+[ "$DEVEL" ] || rm -rf "$BUILDDIR"
+pkg_uninstall
+exit 0
