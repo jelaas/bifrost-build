a9264cc803972a5a75eb960f9a6ad0ee10540c35
Jens Låås <jens@laas.mine.nu>
2011-08-18 12:15:50 +0200

    alt source fetching

--- all/x86info-1.27-1/Fetch-source.sh
+++ all/x86info-1.27-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=x86info-1.27.tgz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://codemonkey.org.uk/projects/x86info/$SRC
+[ -s "$DST" ] || wget -O $DST http://codemonkey.org.uk/projects/x86info/$SRC || wget -O $DST http://codemonkey.org.uk/projects/x86info/old/$SRC
