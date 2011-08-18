c8451241999aa1959edae9f823d82a68b7f331e4
Jens Låås <jens@laas.mine.nu>
2011-08-18 12:07:26 +0200

    alt source locations

--- all/opt-php-5.3.3-1/Fetch-source.sh
+++ all/opt-php-5.3.3-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=php-5.3.3.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://static.php.net/www.php.net/distributions/$SRC
+[ -s "$DST" ] || wget -O $DST http://static.php.net/www.php.net/distributions/$SRC || wget -O $DST http://museum.php.net/php5/$SRC
