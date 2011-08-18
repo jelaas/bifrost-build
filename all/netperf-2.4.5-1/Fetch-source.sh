1039241fee07bc29ddcb0b4bf9a216bb7a017ecb
Jens Låås <jens@laas.mine.nu>
2011-08-18 13:14:34 +0200

    alt source locations

--- all/netperf-2.4.5-1/Fetch-source.sh
+++ all/netperf-2.4.5-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=netperf-2.4.5.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST ftp://ftp.netperf.org/netperf/$SRC
+[ -s "$DST" ] || wget -O $DST ftp://ftp.netperf.org/netperf/$SRC || wget -O $DST ftp://ftp.netperf.org/netperf/archive/$SRC
--- all/opt-emacs-23.2-1/Fetch-source.sh
+++ all/opt-emacs-23.2-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=emacs-23.2.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST ftp://ftp.sunet.se/pub/gnu/emacs/$SRC
+[ -s "$DST" ] || wget -O $DST ftp://ftp.sunet.se/pub/gnu/emacs/$SRC || wget -O $DST ftp://ftp.sunet.se/pub/gnu/emacs/emacs-23.2a.tar.gz
