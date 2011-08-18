71c0ddbb07ea5ef81f1bac13941a4e1b58921580
Jens Låås <jens@laas.mine.nu>
2011-08-18 10:35:50 +0200

    alternate download sites

--- all/opt-apache-2.2.17-1/Fetch-source.sh
+++ all/opt-apache-2.2.17-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=httpd-2.2.17.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://apache.archive.sunet.se/dist/httpd/$SRC || wget -O $DST https://archive.apache.org/dist/httpd/$SRC
+[ -s "$DST" ] || wget -O $DST http://apache.archive.sunet.se/dist/httpd/$SRC || wget --no-check-certificate -O $DST https://archive.apache.org/dist/httpd/$SRC
