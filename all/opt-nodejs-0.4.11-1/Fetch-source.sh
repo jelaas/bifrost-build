87c285b5869c6811dcd43ee20b2012b1505c6162
Jens Låås <jens@laas.mine.nu>
2011-09-06 13:05:43 +0200

    node.js - serverside javascript engine. based on V8

---
+++ all/opt-nodejs-0.4.11-1/Fetch-source.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+VER=0.4.11
+SRC=nodejs-$VER.tar.gz
+DST=/var/spool/src/$SRC
+
+if [ ! -s "$DST" ]; then
+    pkg_install passwd-file-1 || exit 2
+    pkg_install git-1.7.1-2 || exit 2
+    pkg_install openssh-5.5p1-1 || exit 2
+    cd /tmp
+    rm -rf nodejs-$VER
+    /opt/git/bin/git clone --depth 1 git://github.com/joyent/node.git nodejs-$VER || exit 1
+    cd nodejs-$VER
+    /opt/git/bin/git checkout v$VER || exit 1
+    rm -rf .git
+    cd /tmp
+    tar czf $DST nodejs-$VER
+    rm -rf nodejs-$VER
+fi
