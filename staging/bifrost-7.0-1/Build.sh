75ab03391a476ded19f648c89cdc6a7696f336ff
Jens Låås <jens@laas.mine.nu>
2011-09-07 22:17:44 +0200

    update kernel version

--- staging/bifrost-7.0-1/Build.sh
+++ staging/bifrost-7.0-1/Build.sh
@@ -180,8 +180,8 @@ untar wget-1.12-1
 
 # core
 
-untar kernel-x86_32-3.0.0-2
-untar kernel-x86_64-3.0.0-2
+untar kernel-x86_32-3.0.0-5
+untar kernel-x86_64-3.0.0-5
 
 untar bash-4.1-1
 untar binutils-2.20.1-1 ./usr/bin/size
