963f7a33f07a328d8293e2dfd8eb4dad1f9d3b6a
Jens Låås <jens@laas.mine.nu>
2011-09-16 19:47:19 +0200

    lxc compiled against uClibc-0.9.32

---
+++ all/opt-lxc-0.7.5-2/Build.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+SRCVER=lxc-0.7.5
+PKG=opt-$SRCVER-2 # with build version
+
+# PKGDIR is set by 'pkg_build'. Usually "/var/lib/build/all/$PKG".
+PKGDIR=${PKGDIR:-/var/lib/build/all/$PKG}
+SRC=/var/spool/src/$SRCVER.tar.gz
+[ -f /var/spool/src/$SRCVER.tar.bz2 ] && SRC=/var/spool/src/$SRCVER.tar.bz2
+BUILDDIR=/var/tmp/src/$SRCVER
+DST="/var/tmp/install/$PKG"
+
+#########
+# Simple inplace edit with sed.
+# Usage: sedit 's/find/replace/g' Filename
+function sedit {
+    sed "$1" $2 > /tmp/sedit.$$
+    cp /tmp/sedit.$$ $2
+    rm /tmp/sedit.$$
+}
+
+#########
+# Fetch sources
+./Fetch-source.sh || exit $?
+pkg_uninstall # Uninstall any dependencies used by Fetch-source.sh
+
+#########
+# Install dependencies:
+# pkg_available dependency1-1 dependency2-1
+pkg_install autoconf-2.65-1 || exit 2
+pkg_install m4-1.4.14-1 || exit 2
+pkg_install automake-1.11.1-1 || exit 2
+pkg_install perl-5.10.1-1 || exit 2
+pkg_install libcap-2.19-1 || exit 2
+pkg_install uClibc-0.9.32-1 || exit 2
+
+#########
+# Unpack sources into dir under /var/tmp/src
+cd $(dirname $BUILDDIR); tar xf $SRC
+
+#########
+# Patch
+cd $BUILDDIR
+touch ChangeLog
+cp $PKGDIR/Makefile.am src/lxc
+libtool_fix-1
+
+#########
+# Configure
+./autogen.sh || exit 1
+B-configure-1 --prefix=/opt/lxc --localstatedir=/var || exit 1
+[ -f config.log ] && cp -p config.log /var/log/config/$PKG-config.log
+
+#########
+# Post configure patch
+# patch -p0 < $PKGDIR/Makefile.pat
+
+#########
+# Compile
+make || exit 1
+
+#########
+# Install into dir under /var/tmp/install
+rm -rf "$DST"
+make install DESTDIR=$DST # --with-install-prefix may be an alternative
+OPTDIR=$DST/opt/lxc
+mkdir -p $OPTDIR/etc/config.flags
+mkdir -p $OPTDIR/rc.d
+echo yes > $OPTDIR/etc/config.flags/example
+echo $PKG > $OPTDIR/pkgversion
+cp -p $PKGDIR/rc $OPTDIR/rc.d/rc.example
+[ -f $PKGDIR/README ] && cp -p $PKGDIR/README $OPTDIR
+cp -r $PKGDIR/doc $OPTDIR
+
+#########
+# Check result
+cd $DST || exit 1
+(ldd opt/lxc/bin/lxc-start|grep -qs "not a dynamic executable") || exit 1
+
+#########
+# Clean up
+cd $DST || exit 1
+rm -rf $OPTDIR/include
+[ -d $OPTDIR/lib/lxc ] && strip $OPTDIR/lib/lxc/*
+[ -d $OPTDIR/bin ] && strip $OPTDIR/bin/*
+[ -d $OPTDIR/usr/bin ] && strip $OPTDIR/usr/bin/*
+
+#########
+# Make package
+cd $DST
+tar czf /var/spool/pkg/$PKG.tar.gz .
+
+#########
+# Cleanup after a success
+cd /var/lib/build
+[ "$DEVEL" ] || rm -rf "$DST"
+[ "$DEVEL" ] || rm -rf "$BUILDDIR"
+pkg_uninstall
+exit 0
