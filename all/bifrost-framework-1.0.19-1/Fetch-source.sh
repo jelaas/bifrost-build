2845564b19d88b581e17b2f49b6b745a54c5cc2b
Jens Låås <jens@laas.mine.nu>
2011-11-14 15:25:21 +0100

    new framwork release. small bugfixes

---
+++ all/bifrost-framework-1.0.19-1/Fetch-source.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+V=1.0.19
+VER=bifrost-framework-$V
+SRC=bifrost-framework-$V.tar.gz
+DST=/var/spool/src/$SRC
+
+if [ ! -s "$DST" ]; then
+    pkg_install passwd-file-1 || exit 2
+    pkg_install git-1.7.1-2 || exit 2
+    pkg_install openssh-5.5p1-1 || exit 2
+    cd /tmp
+    rm -rf $VER
+    /opt/git/bin/git clone git://github.com/jelaas/bifrost-framework.git $VER || exit 1
+    cd $VER
+    /opt/git/bin/git checkout $V || exit 1
+    cd /tmp
+    tar czf $DST $VER
+    rm -rf $VER
+fi
+
