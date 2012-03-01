7eeffc8f8983170a3e62cc80b0cf9c04db2c1e43
Jens Låås <jens@laas.mine.nu>
2012-03-01 13:08:48 +0100

    apache-netlog version 1.9

---
+++ all/apache-netlog-1.9-1/Build.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+SRCVER=apache-netlog-1.9
+PKG=$SRCVER-1 # with build version
+
+# PKGDIR is set by 'pkg_build'. Usually "/var/lib/build/all/$PKG".
+PKGDIR=${PKGDIR:-/var/lib/build/all/$PKG}
+SRC=/var/spool/src/$SRCVER.tar.gz
+BUILDDIR=/var/tmp/src/$SRCVER
+DST="/var/tmp/install/$PKG"
+
+#########
+# Simple inplace edit with sed.
+# Usage: sedit 's/find/replace/g' Filename
+function sedit {
+    sed "$1" $2 > /tmp/sedit.$$
+    cp /tmp/sedit.$$ $2
+    rm /tmp/sedit.$$
+}
+
+#########
+# Fetch sources
+./Fetch-source.sh || exit $?
+pkg_uninstall # Uninstall any dependencies used by Fetch-source.sh
+
+#########
+# Install dependencies:
+# pkg_available dependency1-1 dependency2-1
+pkg_install zlib-1.2.6-1 || exit 2
+pkg_install curl-devel-7.20.1-1 || exit 2
+pkg_install openssl-0.9.8t-1 || exit 2
+pkg_install git-1.7.1-2 || exit 2
+
+#########
+# Unpack sources into dir under /var/tmp/src
+cd $(dirname $BUILDDIR); tar xf $SRC
+
+#########
+# Patch
+cd $BUILDDIR || exit 1
+sed -i 's/-lidn//' Makefile  || exit 1
+sed -i 's/-lrtmp//' Makefile  || exit 1
+sed -i 's/-Wall/-Wall -D_GNU_SOURCE/' Makefile || exit 1
+
+#########
+# Configure
+#B-configure-1 --prefix=/usr || exit 1
+#[ -f config.log ] && cp -p config.log /var/log/config/$PKG-config.log
+
+#########
+# Post configure patch
+# patch -p0 < $PKGDIR/Makefile.pat
+
+#########
+# Compile
+export PATH=$PATH:/opt/git/bin/
+LDFLAGS=-static make || exit 1
+
+#########
+# Install into dir under /var/tmp/install
+rm -rf "$DST"
+make install DESTDIR=$DST # --with-install-prefix may be an alternative
+
+#########
+# Check result
+cd $DST || exit 1
+# [ -f usr/bin/myprog ] || exit 1
+(ldd usr/bin/apache-netlog|grep -qs "not a dynamic executable") || exit 1
+
+#########
+# Clean up
+cd $DST || exit 1
+# rm -rf usr/share usr/man
+[ -d bin ] && strip bin/*
+[ -d usr/bin ] && strip usr/bin/*
+
+#########
+# Make package
+cd $DST || exit 1
+tar czf /var/spool/pkg/$PKG.tar.gz .
+
+#########
+# Cleanup after a success
+cd /var/lib/build
+[ "$DEVEL" ] || rm -rf "$DST"
+[ "$DEVEL" ] || rm -rf "$BUILDDIR"
+pkg_uninstall
+exit 0
