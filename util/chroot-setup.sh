4ebc0158656d0a88f679855e3aa50eb05b8c00c0
Jens Låås <jens@laas.mine.nu>
2012-04-23 08:42:35 +0200

    make fancier setup

--- util/chroot-setup.sh
+++ util/chroot-setup.sh
@@ -4,9 +4,9 @@
 [ -d /sys/class ] || mount -t sysfs sys /sys
 [ -c /dev/urandom ] || mdev -s
 
-[ -d /proc/self/fd ] && ln -s /proc/self/fd /dev/fd
-[ -L /dev/fd/0 ] && ln -s /dev/fd/0 /dev/stdin
-[ -L /dev/fd/1 ] && ln -s /dev/fd/1 /dev/stdout
-[ -L /dev/fd/2 ] && ln -s /dev/fd/2 /dev/stderr
+[ -d /proc/self/fd ] && [ ! -e /dev/fd ] && ln -s /proc/self/fd /dev/fd
+[ -L /dev/fd/0 ] && [ ! -e /dev/stdin ] && ln -s /dev/fd/0 /dev/stdin
+[ -L /dev/fd/1 ] && [ ! -e /dev/stdout ] && ln -s /dev/fd/1 /dev/stdout
+[ -L /dev/fd/2 ] && [ ! -e /dev/stderr ] && ln -s /dev/fd/2 /dev/stderr
 
 exec /bin/bash -li
