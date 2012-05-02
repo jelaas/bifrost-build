b61002270b9fb354c4a0b671f34b78573fe2b87e
Jens Låås <jens@laas.mine.nu>
2012-05-02 15:52:31 +0200

    moved to buildversion 4

--- all/openssh-5.5p1-4/Build.sh
+++ all/openssh-5.5p1-4/Build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SRCVER=openssh-5.5p1
-PKG=$SRCVER-3 # with build version
+PKG=$SRCVER-4 # with build version
 
 PKGDIR=${PKGDIR:-/var/lib/build/all/$PKG}
 SRC=/var/spool/src/$SRCVER.tar.gz
