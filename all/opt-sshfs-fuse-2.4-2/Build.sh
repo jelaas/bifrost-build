f8d6de929e79651e2ba9ee67a7524fc2ea103ac1
Jens Låås <jens@laas.mine.nu>
2012-05-04 14:38:23 +0200

    working rc.sshfs etc

---
+++ all/opt-sshfs-fuse-2.4-2/Build.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+SRCVER=sshfs-fuse-2.4
+PKG=opt-$SRCVER-2 # with build version
+
+# PKGDIR is set by 'pkg_build'. Usually "/var/lib/build/all/$PKG".
+PKGDIR=${PKGDIR:-/var/lib/build/all/$PKG}
+SRC=/var/spool/src/$SRCVER.tar.gz
+[ -f /var/spool/src/$SRCVER.tar.bz2 ] && SRC=/var/spool/src/$SRCVER.tar.bz2
+BUILDDIR=/var/tmp/src/$SRCVER
+DST="/var/tmp/install/$PKG"
+
+#########
+# Simple inplace edit with sed.
+# Usage: sedit 's/find/replace/g' Filename
+function sedit {
+    sed "$1" $2 > /tmp/sedit.$$
+    cp /tmp/sedit.$$ $2
+    rm /tmp/sedit.$$
+}
+
+#########
+# Fetch sources
+./Fetch-source.sh || exit $?
+pkg_uninstall # Uninstall any dependencies used by Fetch-source.sh
+
+#########
+# Install dependencies:
+# pkg_available dependency1-1 dependency2-1
+# pkg_install dependency1-1 || exit 2
+pkg_install groff-1.21-1 || exit 2 # Needed to convert man-pages: see below
+pkg_install pkg-config-0.23-1 || exit 2
+pkg_install glib-2.24.2-1 || exit 2
+pkg_install fuse-2.9.0-1 || exit 2
+pkg_install libiconv-1.13.1-1 || exit 2
+
+#########
+# Unpack sources into dir under /var/tmp/src
+cd $(dirname $BUILDDIR); tar xf $SRC
+
+#########
+# Patch
+cd $BUILDDIR || exit 1
+libtool_fix-1
+# patch -p1 < $PKGDIR/mypatch.pat
+
+#########
+# Configure
+OPTPREFIX=opt/sshfs
+SSHFS_LIBS="-lfuse -lgthread-2.0 -lpthread -lglib-2.0 -liconv" B-configure-1 --prefix=/$OPTPREFIX --localstatedir=/var || exit 1
+[ -f config.log ] && cp -p config.log /var/log/config/$PKG-config.log
+
+#########
+# Post configure patch
+# patch -p0 < $PKGDIR/Makefile.pat
+
+#########
+# Compile
+make || exit 1
+
+#########
+# Install into dir under /var/tmp/install
+rm -rf "$DST"
+make install DESTDIR=$DST # --with-install-prefix may be an alternative
+OPTDIR=$DST/$OPTPREFIX
+mkdir -p $OPTDIR/etc/config.flags
+mkdir -p $OPTDIR/etc/config.preconf
+cp $PKGDIR/remote-mounts $OPTDIR/etc/config.preconf
+cp $PKGDIR/remote-account $OPTDIR/etc/config.preconf
+cp $PKGDIR/mount-options $OPTDIR/etc/config.preconf
+mkdir -p $OPTDIR/rc.d
+echo yes > $OPTDIR/etc/config.flags/sshfs
+echo $PKG > $OPTDIR/pkgversion
+cp -p $PKGDIR/rc $OPTDIR/rc.d/rc.sshfs
+chmod +x $OPTDIR/rc.d/rc.sshfs
+[ -f $PKGDIR/README ] && cp -p $PKGDIR/README $OPTDIR
+
+#########
+# Convert man-pages
+cd $DST || exit 1
+for f in $(find . -path \*man/man\*); do if [ -f $f ]; then groff -T utf8 -man $f > $f.txt; rm $f; fi; done
+
+#########
+# Check result
+cd $DST || exit 1
+# [ -f usr/bin/myprog ] || exit 1
+# (ldd sbin/myprog|grep -qs "not a dynamic executable") || exit 1
+
+#########
+# Clean up
+cd $DST || exit 1
+# rm -rf usr/share usr/man
+[ -d $OPTPREFIX/bin ] && strip $OPTPREFIX/bin/*
+[ -d $OPTPREFIX/sbin ] && strip $OPTPREFIX/sbin/*
+[ -d $OPTPREFIX/usr/bin ] && strip $OPTPREFIX/usr/bin/*
+
+#########
+# Make package
+cd $DST
+tar czf /var/spool/pkg/$PKG.tar.gz .
+
+#########
+# Cleanup after a success
+cd /var/lib/build
+[ "$DEVEL" ] || rm -rf "$DST"
+[ "$DEVEL" ] || rm -rf "$BUILDDIR"
+pkg_uninstall
+exit 0
