301041d171eaaad0dc29b8884ee0079927b18159
Jens Låås <jens@laas.mine.nu>
2012-06-21 15:47:42 +0200

    php upgrade

---
+++ all/opt-php-5.3.14-1/Fetch-source.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SRC=php-5.3.14.tar.bz2
+DST=/var/spool/src/$SRC
+
+[ -s "$DST" ] || wget -O $DST http://static.php.net/www.php.net/distributions/$SRC || wget -O $DST http://museum.php.net/php5/$SRC
