baa61762e17b73001bd0160eeebf25e687e1664c
Jens Låås <jens@laas.mine.nu>
2012-06-29 16:14:42 +0200

    fixed source locations

--- all/e3-2.8-1/Fetch-source.sh
+++ all/e3-2.8-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=e3-2.8.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://sites.google.com/site/e3editor/Home/e3-2.8.tgz
+[ -s "$DST" ] || wget --no-check-certificate -O $DST http://sites.google.com/site/e3editor/Home/e3-2.8.tgz
