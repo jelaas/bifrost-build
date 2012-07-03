1bc6a5e8fa222f3849da5148492a4f84802e42b5
Jens Låås <jens@laas.mine.nu>
2012-07-03 16:03:42 +0200

    pcre for musl

---
+++ all/musl-pcre-8.02-1/Build.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+SRCVER=pcre-8.02
+PKG=musl-$SRCVER-1 # with build version
+
+PKGDIR=${PKGDIR:-/var/lib/build/all/$PKG}
+SRC=/var/spool/src/$SRCVER.tar.bz2
+CDIR=/var/tmp/src
+DST="/var/tmp/install/$PKG"
+
+#########
+# Install dependencies:
+pkg_install musl-0.9.1-1 || exit 1
+
+#########
+# Unpack sources into dir under /var/tmp/src
+./Fetch-source.sh || exit 1
+cd $CDIR; tar xf $SRC
+
+#########
+# Patch
+cd $CDIR/$SRCVER
+libtool_fix-1
+# patch -p1 < $PKGDIR/mypatch.pat
+
+#########
+# Configure
+CC=musl-gcc B-configure-1 --prefix=/opt/musl --disable-cpp --enable-utf8 --enable-unicode-properties|| exit 1
+
+#########
+# Post configure patch
+# patch -p0 < $PKGDIR/Makefile.pat
+
+#########
+# Compile
+make -j || exit 1
+
+#########
+# Install into dir under /var/tmp/install
+rm -rf "$DST"
+make install DESTDIR=$DST # --with-install-prefix may be an alternative
+
+#########
+# Check result
+cd $DST
+# [ -f usr/bin/myprog ] || exit 1
+# (file usr/bin/myprog | grep -qs "statically linked") || exit 1
+
+#########
+# Clean up
+cd $DST
+# rm -rf usr/share usr/man
+rm -rf opt/musl/share
+rm -f opt/musl/bin/pcretest opt/musl/bin/pcregrep
+[ -d bin ] && strip bin/*
+[ -d usr/bin ] && strip usr/bin/*
+
+#########
+# Make package
+cd $DST
+tar czf /var/spool/pkg/$PKG.tar.gz .
+
+#########
+# Cleanup after a success
+cd /var/lib/build
+[ "$DEVEL" ] || rm -rf "$DST"
+[ "$DEVEL" ] || rm -rf "$CDIR/$SRCVER"
+pkg_uninstall
+exit 0
