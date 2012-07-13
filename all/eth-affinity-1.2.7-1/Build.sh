c18f8b2514d55bd190a3a46ee8153f5ca6d24760
Jens Låås <jens@laas.mine.nu>
2012-07-13 09:37:27 +0200

    eth-affinity 1.2.7

---
+++ all/eth-affinity-1.2.7-1/Build.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+SRCVER=eth-affinity-1.2.7
+PKG=$SRCVER-1 # with build version
+
+# PKGDIR is set by 'pkg_build'. Usually "/var/lib/build/all/$PKG".
+PKGDIR=${PKGDIR:-/var/lib/build/all/$PKG}
+SRC=/var/spool/src/$SRCVER.tar.gz
+BUILDDIR=/var/tmp/src/$SRCVER
+DST="/var/tmp/install/$PKG"
+
+#########
+# Simple inplace edit with sed.
+# Usage: sedit 's/find/replace/g' Filename
+function sedit {
+    sed "$1" $2 > /tmp/sedit.$$
+    cp /tmp/sedit.$$ $2
+    rm /tmp/sedit.$$
+}
+
+#########
+# Install dependencies:
+pkg_available passwd-file-1 dietlibc-0.32-1 group-file-1
+pkg_install passwd-file-1 || exit 2
+pkg_install dietlibc-0.32-1 || exit 2
+pkg_install group-file-1 || exit 2
+
+#########
+# Unpack sources into dir under /var/tmp/src
+./Fetch-source.sh || exit $?
+cd $(dirname $BUILDDIR); tar xf $SRC
+
+#########
+# Patch
+cd $BUILDDIR
+libtool_fix-1
+# patch -p1 < $PKGDIR/mypatch.pat
+
+#########
+# Configure
+B-configure-1 --diet --prefix=$DST || exit 1
+
+#########
+# Post configure patch
+# patch -p0 < $PKGDIR/Makefile.pat
+
+#########
+# Compile
+PATH=/opt/diet/bin:$PATH make || exit 1
+
+#########
+# Install into dir under /var/tmp/install
+rm -rf "$DST"
+mkdir -p $DST/bin
+make install
+mv $DST/bin $DST/sbin
+
+#########
+# Check result
+cd $DST
+# [ -f usr/bin/myprog ] || exit 1
+# (ldd sbin/myprog|grep -qs "not a dynamic executable") || exit 1
+
+#########
+# Clean up
+cd $DST
+# rm -rf usr/share usr/man
+[ -d bin ] && strip bin/*
+[ -d usr/bin ] && strip usr/bin/*
+[ -d usr/sbin ] && strip usr/sbin/*
+
+#########
+# Make package
+cd $DST
+tar czf /var/spool/pkg/$PKG.tar.gz .
+
+#########
+# Cleanup after a success
+cd /var/lib/build
+[ "$DEVEL" ] || rm -rf "$DST"
+[ "$DEVEL" ] || rm -rf "$BUILDDIR"
+pkg_uninstall
+exit 0
