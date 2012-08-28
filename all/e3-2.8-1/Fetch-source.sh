563ae1aeb1503e4ddb33e5f041d153e41d0c1abb
Jens Låås <jens@laas.mine.nu>
2012-08-28 11:10:08 +0200

    need wget for --no-check-certificate

--- all/e3-2.8-1/Fetch-source.sh
+++ all/e3-2.8-1/Fetch-source.sh
@@ -3,4 +3,8 @@
 SRC=e3-2.8.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget --no-check-certificate -O $DST http://sites.google.com/site/e3editor/Home/e3-2.8.tgz
+if [ ! -s "$DST" ]; then
+	pkg_install wget-1.12-2
+	wget --no-check-certificate -O $DST http://sites.google.com/site/e3editor/Home/e3-2.8.tgz
+fi
+
