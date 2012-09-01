d9f8e90addec579b15cd7133e29ff1fe724afdf2
Jens Låås <jens@laas.mine.nu>
2012-09-02 00:09:49 +0200

    src address

--- all/bridge-utils-1.4-1/Fetch-source.sh
+++ all/bridge-utils-1.4-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=bridge-utils-1.4.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://kent.dl.sourceforge.net/project/bridge/bridge/bridge-utils-1.4/$SRC
+[ -s "$DST" ] || wget -O $DST http://downloads.sourceforge.net/project/bridge/bridge/bridge-utils-1.4/$SRC
