adc892107cdc8c3395f6df341f8536fc5b692fd7
Jens Låås <jens@laas.mine.nu>
2012-09-02 00:30:01 +0200

    source location moved

--- all/zlib-1.2.5-1/Fetch-source.sh
+++ all/zlib-1.2.5-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=zlib-1.2.5.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://www.zlib.net/$SRC || wget -O $DST http://ftp.sunet.se/pub/os/Linux/distributions/bifrost/download/src/$SRC || wget -O $DST ftp://ftp.simplesystems.org/pub/libpng/png/src/history/$SRC
+[ -s "$DST" ] || wget -O $DST http://www.zlib.net/$SRC || wget -O $DST http://ftp.sunet.se/pub/os/Linux/distributions/bifrost/download/src/$SRC || wget -O $DST ftp://ftp.simplesystems.org/pub/libpng/png/src/history/zlib/$SRC
