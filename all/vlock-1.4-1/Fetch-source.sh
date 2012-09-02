3604d42b1cb173999bf67bd95bdc90a799267aed
Jens Låås <jens@laas.mine.nu>
2012-09-02 20:07:35 +0200

    add ftp.sunet.se as backup for source

--- all/vlock-1.4-1/Fetch-source.sh
+++ all/vlock-1.4-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=vlock-1.4.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://cthulhu.c3d2.de/~toidinamai/vlock/archive/$SRC
+[ -s "$DST" ] || wget -O $DST http://cthulhu.c3d2.de/~toidinamai/vlock/archive/$SRC || wget -O $DST http://ftp.sunet.se/pub/os/Linux/distributions/bifrost/download/src/$SRC
