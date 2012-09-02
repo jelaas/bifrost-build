ae044df53117e3c942f7f91ebfe24677a0f44f4a
Jens Låås <jens@laas.mine.nu>
2012-09-02 09:25:31 +0200

    source location corrections

--- all/libnet-1.1.4-1/Fetch-source.sh
+++ all/libnet-1.1.4-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=libnet-1.1.4.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://garr.dl.sourceforge.net/project/libnet-dev/libnet-dev/libnet-1.1.4/$SRC || wget -O $DST http://fastbull.dl.sourceforge.net/project/libnet-dev/libnet-dev/libnet-1.1.4/$SRC
+[ -s "$DST" ] || wget -O $DST http://downloads.sourceforge.net/project/libnet-dev/libnet-dev/libnet-1.1.4/$SRC
