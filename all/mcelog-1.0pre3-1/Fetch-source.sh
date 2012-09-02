d5ecdecffdcf90a52121e53e5af5b9621fc01b37
Jens Låås <jens@laas.mine.nu>
2012-09-02 08:38:23 +0200

    source location corrections

--- all/mcelog-1.0pre3-1/Fetch-source.sh
+++ all/mcelog-1.0pre3-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=mcelog-1.0pre3.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://www.kernel.org/pub/linux/utils/cpu/mce/$SRC || wget -O $DST http://ftp.sunet.se/pub/Linux/distributions/bifrost/src/$SRC
+[ -s "$DST" ] || wget -O $DST http://www.kernel.org/pub/linux/utils/cpu/mce/$SRC || wget -O $DST http://ftp.sunet.se/pub/Linux/distributions/bifrost/download/src/$SRC
