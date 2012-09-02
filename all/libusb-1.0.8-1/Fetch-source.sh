b9b9f6abe9af9b0dac9fb5c28a3b7d4dc61fd47a
Jens Låås <jens@laas.mine.nu>
2012-09-02 09:18:35 +0200

    source location corrections

--- all/libusb-1.0.8-1/Fetch-source.sh
+++ all/libusb-1.0.8-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=libusb-1.0.8.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://kent.dl.sourceforge.net/project/libusb/libusb-1.0/libusb-1.0.8/$SRC
+[ -s "$DST" ] || wget -O $DST http://downloads.sourceforge.net/project/libusb/libusb-1.0/libusb-1.0.8/$SRC
