ce454a1b24e2ac4032e93fa849460283f0b5b5a3
Jens Låås <jens@laas.mine.nu>
2012-09-02 19:07:28 +0200

    adjust source location

--- all/psmisc-22.11-1/Fetch-source.sh
+++ all/psmisc-22.11-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=psmisc-22.11.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://downloads.sourceforge.net/project/psmisc/psmisc/$SRC
+[ -s "$DST" ] || wget -O $DST http://downloads.sourceforge.net/project/psmisc/psmisc/Archive/$SRC || wget -O $DST http://downloads.sourceforge.net/project/psmisc/psmisc/$SRC
