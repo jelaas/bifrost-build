eac90f69b0fb82e999c824d76f7d08989f4265ec
Jens Låås <jens@laas.mine.nu>
2012-09-02 08:51:29 +0200

    source location corrections

--- all/pcre-8.02-1/Fetch-source.sh
+++ all/pcre-8.02-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=pcre-8.02.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/$SRC || wget -O $DST http://kent.dl.sourceforge.net/project/pcre/pcre/8.02/$SRC
+[ -s "$DST" ] || wget -O $DST ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/$SRC || wget -O $DST http://downloads.sourceforge.net/project/pcre/pcre/8.02/$SRC
