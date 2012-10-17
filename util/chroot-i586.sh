fa5d1346e8031a88680e83e799feac618c2d8954
Jens Låås <jens@laas.mine.nu>
2012-10-17 11:01:09 +0200

    build environment now relocatable

--- util/chroot-i586.sh
+++ util/chroot-i586.sh
@@ -1,4 +1,8 @@
-mkdir -p /home/build/mini-native-i586/var/lib
-cp /etc/resolv.conf /home/build/mini-native-i586/etc
-cp /etc/hosts /home/build/mini-native-i586/etc
-setarch i586 chroot /home/build/mini-native-i586 /bin/chroot-setup.sh
+[ $(id -u) == 0 ] || { printf "\n  Oops, needs wo be run as root due to chroot\n\n"; exit 1; }
+
+BASEDIR=$( dirname $( readlink -f $0 ) )
+
+mkdir -p $BASEDIR/mini-native-i586/var/lib
+cp /etc/resolv.conf $BASEDIR/mini-native-i586/etc
+cp /etc/hosts $BASEDIR/mini-native-i586/etc
+setarch i586 chroot $BASEDIR/mini-native-i586 /bin/chroot-setup.sh
