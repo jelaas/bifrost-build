533df4e8c26d95a64da3577dc6542aea5f79a2f5
Jens Låås <jens@laas.mine.nu>
2013-02-28 18:51:37 +0100

    grub2 for bifrost

---
+++ all/grub-1.99-2/Build.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+SRCVER=grub-1.99
+PKG=$SRCVER-2 # with build version
+
+# PKGDIR is set by 'pkg_build'. Usually "/var/lib/build/all/$PKG".
+PKGDIR=${PKGDIR:-/var/lib/build/all/$PKG}
+SRC=/var/spool/src/$SRCVER.tar.gz
+[ -f /var/spool/src/$SRCVER.tar.bz2 ] && SRC=/var/spool/src/$SRCVER.tar.bz2
+BUILDDIR=/var/tmp/src/$SRCVER
+DST="/var/tmp/install/$PKG"
+
+#########
+# Simple inplace edit with sed.
+# Usage: sedit 's/find/replace/g' Filename
+function sedit {
+    sed "$1" $2 > /tmp/sedit.$$
+    cp /tmp/sedit.$$ $2
+    rm /tmp/sedit.$$
+}
+
+#########
+# Fetch sources
+./Fetch-source.sh || exit $?
+pkg_uninstall # Uninstall any dependencies used by Fetch-source.sh
+
+#########
+# Install dependencies:
+# pkg_available dependency1-1 dependency2-1
+# pkg_install dependency1-1 || exit 2
+pkg_install bison-2.4.2-1 || exit 2
+pkg_install flex-2.5.35-1 || exit 2
+pkg_install m4-1.4.14-1 || exit 2
+
+#########
+# Unpack sources into dir under /var/tmp/src
+cd $(dirname $BUILDDIR); tar xf $SRC
+
+#########
+# Patch
+cd $BUILDDIR || exit 1
+libtool_fix-1
+patch -p0 < $PKGDIR/misc.c.pat || exit 1
+
+#########
+# Configure
+B-configure-1 --prefix=/ || exit 1
+[ -f config.log ] && cp -p config.log /var/log/config/$PKG-config.log
+
+#########
+# Post configure patch
+# patch -p0 < $PKGDIR/Makefile.pat
+
+#########
+# Compile
+make || exit 1
+
+#########
+# Install into dir under /var/tmp/install
+rm -rf "$DST"
+make install DESTDIR=$DST # --with-install-prefix may be an alternative
+
+#########
+# Check result
+cd $DST || exit 1
+# [ -f usr/bin/myprog ] || exit 1
+# (ldd sbin/myprog|grep -qs "not a dynamic executable") || exit 1
+
+#########
+# Clean up
+cd $DST || exit 1
+rm -rf share etc/bash_completion.d
+rm -f etc/grub.d/10_linux etc/grub.d/20_linux_xen etc/grub.d/30_os-prober etc/grub.d/40_custom
+cp -f $PKGDIR/10_bifrost etc/grub.d
+
+[ -d bin ] && strip bin/*
+[ -d sbin ] && strip sbin/*
+
+#########
+# Make package
+cd $DST || exit 1
+tar czf /var/spool/pkg/$PKG.tar.gz .
+
+#########
+# Cleanup after a success
+cd /var/lib/build
+[ "$DEVEL" ] || rm -rf "$DST"
+[ "$DEVEL" ] || rm -rf "$BUILDDIR"
+pkg_uninstall
+exit 0
