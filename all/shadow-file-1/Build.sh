d6c217db7c0a3cc7c4682584280a57ae8d2ba40d
Jens Låås <jens@laas.mine.nu>
2013-03-19 17:48:56 +0100

    /etc/shadow

---
+++ all/shadow-file-1/Build.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+SRCVER=shadow-file
+PKG=$SRCVER-1 # with build version
+
+PKGDIR=${PKGDIR:-/var/lib/build/all/$PKG}
+SRC=/var/spool/src/$SRCVER.tar.gz
+BUILDDIR=/var/tmp/src/$SRCVER
+DST="/var/tmp/install/$PKG"
+
+#########
+# Install dependencies:
+# pkg_install dependency-1.1 || exit 1
+
+#########
+# Unpack sources into dir under /var/tmp/src
+
+#########
+# Patch
+
+#########
+# Configure
+
+#########
+# Post configure patch
+
+#########
+# Compile
+
+#########
+# Install into dir under /var/tmp/install
+rm -rf "$DST"
+mkdir -p $DST/etc
+cp -p $PKGDIR/shadow $DST/etc
+
+#########
+# Check result
+cd $DST
+# [ -f usr/bin/myprog ] || exit 1
+# (file usr/bin/myprog | grep -qs "statically linked") || exit 1
+
+#########
+# Clean up
+cd $DST
+
+#########
+# Make package
+cd $DST
+tar czf /var/spool/pkg/$PKG.tar.gz .
+
+#########
+# Cleanup after a success
+cd /var/lib/build
+[ "$DEVEL" ] || rm -rf "$DST"
+pkg_uninstall
+exit 0
