41c5a15121a85b3ee52c9b9c47e2211c1accde35
Jens Låås <jens@laas.mine.nu>
2013-05-06 14:34:17 +0200

    build against musl 0.9.10

---
+++ musl-ncurses-lib-5.7-3/Build.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+SRCVER=ncurses-5.7
+PKG=musl-ncurses-lib-5.7-3 # with build version
+
+PKGDIR=${PKGDIR:-/var/lib/build/all/$PKG}
+SRC=/var/spool/src/$SRCVER.tar.gz
+BUILDDIR=/var/tmp/src/$SRCVER
+DST="/var/tmp/install/$PKG"
+
+#########
+# Install dependencies:
+# pkg_available dependency1-1 dependency2-1
+# pkg_install dependency1-1 || exit 1
+pkg_install musl-0.9.10-1 || exit 2 
+export CC=musl-gcc
+
+#########
+# Unpack sources into dir under /var/tmp/src
+./Fetch-source.sh || exit 1
+cd $(dirname $BUILDDIR); tar xf $SRC
+
+#########
+# Patch
+cd $BUILDDIR || exit 1
+libtool_fix-1
+# patch -p1 < $PKGDIR/mypatch.pat
+
+#########
+# Configure
+B-configure-2 --enable-widec --prefix=/opt/musl --datadir=/usr/share --bindir=/usr/bin || exit 1
+
+#########
+# Post configure patch
+# patch -p0 < $PKGDIR/Makefile.pat
+
+#########
+# Compile
+make -j || exit 1
+
+#########
+# Install into dir under /var/tmp/install
+rm -rf "$DST"
+make install DESTDIR=$DST # --with-install-prefix may be an alternative
+
+#########
+# Check result
+cd $DST || exit 1
+# [ -f usr/bin/myprog ] || exit 1
+# (file usr/bin/myprog | grep -qs "statically linked") || exit 1
+
+#########
+# Clean up
+cd $DST
+rm -rf opt/musl/man
+chmod +x usr/bin/ncursesw5-config
+
+#########
+# Make package
+cd $DST
+tar czf /var/spool/pkg/$PKG.tar.gz .
+
+#########
+# Cleanup after a success
+cd /var/lib/build
+[ "$DEVEL" ] || rm -rf "$DST"
+[ "$DEVEL" ] || rm -rf "$BUILDDIR"
+pkg_uninstall
+exit 0
