509be6a5ac04cc01525d7d59cedd637d6cc7e5fb
Jens Låås <jens@laas.mine.nu>
2013-05-07 14:36:15 +0200

    removed device.map

---
+++ all/make-bifrost-image-3/Build.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+SRCVER=make-bifrost-image
+PKG=$SRCVER-3 # with build version
+
+# PKGDIR is set by 'pkg_build'. Usually "/var/lib/build/all/$PKG".
+PKGDIR=${PKGDIR:-/var/lib/build/all/$PKG}
+SRC=/var/spool/src/$SRCVER.tar.gz
+[ -f /var/spool/src/$SRCVER.tar.bz2 ] && SRC=/var/spool/src/$SRCVER.tar.bz2
+BUILDDIR=/var/tmp/src/$SRCVER
+DST="/var/tmp/install/$PKG"
+
+#########
+# Simple inplace edit with sed.
+# Usage: sedit 's/find/replace/g' Filename
+function sedit {
+    sed "$1" $2 > /tmp/sedit.$$
+    cp /tmp/sedit.$$ $2
+    rm /tmp/sedit.$$
+}
+
+#########
+# Fetch sources
+./Fetch-source.sh || exit $?
+pkg_uninstall # Uninstall any dependencies used by Fetch-source.sh
+
+#########
+# Install dependencies:
+# pkg_available dependency1-1 dependency2-1
+# pkg_install dependency1-1 || exit 2
+# pkg_install groff-1.21-1 || exit 2 # Needed to convert man-pages: see below
+
+# Compile against musl:
+# pkg_install musl-0.9.1-1 || exit 2 
+# export CC=musl-gcc
+
+#########
+# Install into dir under /var/tmp/install
+rm -rf "$DST"
+mkdir -p "$DST" || exit 1
+mkdir -p $DST/boot
+mkdir -p $DST/
+mkdir -p $DST/sbin
+mkdir -p $DST/Documentation
+
+cp "$PKGDIR/make-bifrost-image" $DST/sbin
+
+#########
+# Convert man-pages
+cd $DST || exit 1
+# for f in $(find . -path \*man/man\*); do if [ -f $f ]; then groff -T utf8 -man $f > $f.txt; rm $f; fi; done
+
+#########
+# Check result
+cd $DST || exit 1
+# [ -f usr/bin/myprog ] || exit 1
+# (ldd sbin/myprog|grep -qs "not a dynamic executable") || exit 1
+
+#########
+# Clean up
+cd $DST || exit 1
+# rm -rf usr/share usr/man
+
+#########
+# Make package
+cd $DST || exit 1
+tar czf /var/spool/pkg/$PKG.tar.gz .
+
+#########
+# Cleanup after a success
+cd /var/lib/build
+[ "$DEVEL" ] || rm -rf "$DST"
+[ "$DEVEL" ] || rm -rf "$BUILDDIR"
+pkg_uninstall
+exit 0
