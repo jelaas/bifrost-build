6c315719c0ae8e7b0b07fef366cd2f41a9a425a5
Jens Låås <jens@laas.mine.nu>
2013-08-28 10:30:44 +0200

    change src url

--- all/sysfsutils-2.1.0-1/Fetch-source.sh
+++ all/sysfsutils-2.1.0-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=sysfsutils-2.1.0.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://ignum.dl.sourceforge.net/project/linux-diag/sysfsutils/2.1.0/$SRC
+[ -s "$DST" ] || wget -O $DST http://downloads.sourceforge.net/project/linux-diag/sysfsutils/2.1.0/$SRC
