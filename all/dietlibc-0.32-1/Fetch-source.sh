35e07b1442ab75a9104ae4ac9569b227b27ce67c
Jens Låås <jens@laas.mine.nu>
2014-05-27 16:34:33 +0200

    update source

--- all/dietlibc-0.32-1/Fetch-source.sh
+++ all/dietlibc-0.32-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=dietlibc-0.32.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://www.kernel.org/pub/linux/libs/dietlibc/$SRC || wget -O $DST http://ftp.sunet.se/pub/Linux/kernel.org/linux/libs/dietlibc/$SRC
+[ -s "$DST" ] || wget -O $DST http://pkgs.fedoraproject.org/repo/pkgs/dietlibc/dietlibc-0.32.tar.bz2/0098761c17924c15e21d25acdda4a8b5/$SRC || wget -O $DST http://www.kernel.org/pub/linux/libs/dietlibc/$SRC || wget -O $DST http://ftp.sunet.se/pub/Linux/kernel.org/linux/libs/dietlibc/$SRC
