cfb31a5f471ce1822aaf32b15e180bd6323eadbc
Jens Låås <jens@laas.mine.nu>
2014-05-28 15:52:38 +0200

    update source locations

--- all/Python-2.7-2/Fetch-source.sh
+++ all/Python-2.7-2/Fetch-source.sh
@@ -3,4 +3,7 @@
 SRC=Python-2.7.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://www.python.org/ftp/python/2.7/$SRC
+if [ ! -s "$DST" ]; then
+    pkg_install wget-1.12-2 || exit 2
+    wget --no-check-certificate -O $DST https://www.python.org/ftp/python/2.7/$SRC
+fi
