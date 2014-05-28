2a6134fdf01f3e71932c4d708b8bcfb1f7139ea5
Jens Låås <jens@laas.mine.nu>
2014-05-28 13:51:12 +0200

    update source

--- all/usbutils-001-1/Fetch-source.sh
+++ all/usbutils-001-1/Fetch-source.sh
@@ -3,5 +3,5 @@
 SRC=usbutils-001.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST ftp://ftp.kernel.org/pub/linux/utils/usb/usbutils/$SRC || wget -O $DST http://ftp.sunet.se/pub/Linux/kernel.org/linux/utils/usb/usbutils/$SRC
+[ -s "$DST" ] || wget -O $DST http://mirror.linux.org.au/linux/utils/usb/usbutils/$SRC || wget -O $DST ftp://ftp.kernel.org/pub/linux/utils/usb/usbutils/$SRC || wget -O $DST http://ftp.sunet.se/pub/Linux/kernel.org/linux/utils/usb/usbutils/$SRC
 
