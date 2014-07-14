35e81b6cc2e32d2fea218a353123bdc9a99cb438
Jens Låås <jens@laas.mine.nu>
2014-07-14 15:26:21 +0200

    whois + changlog

--- all/bifrost-7.1.1-1/Build.sh
+++ all/bifrost-7.1.1-1/Build.sh
@@ -94,7 +94,7 @@ untar net-tools-1.60-2
 untar ntp-4.2.6p5-1
 untar openssh-6.1p1-1
 untar traceroute-2.0.16-1; chmod +s ./bin/traceroute
-untar whois_5.0.10-1
+untar whois_5.0.10-2
 untar wireless_tools.29-1
 
 # fileutils
