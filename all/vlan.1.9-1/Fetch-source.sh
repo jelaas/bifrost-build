7dbd8f6c5b265bc71bb04a14a98deccc3bcb802c
Jens Låås <jens@laas.mine.nu>
2015-03-06 18:30:57 +0100

    adjust alt source

--- all/vlan.1.9-1/Fetch-source.sh
+++ all/vlan.1.9-1/Fetch-source.sh
@@ -5,5 +5,5 @@ DST=/var/spool/src/$SRC
 
 if [ ! -s "$DST" ]; then
     pkg_install wget-1.12-1 || exit 2
-    wget -O $DST --no-check-certificate  https://laas.mine.nu/srcrepo/$SRC || wget -O $DST http://www.candelatech.com/~greear/vlan/$SRC
+    wget -O $DST --no-check-certificate  http://raemmen.eu/srcrepo/$SRC || wget -O $DST http://www.candelatech.com/~greear/vlan/$SRC
 fi
