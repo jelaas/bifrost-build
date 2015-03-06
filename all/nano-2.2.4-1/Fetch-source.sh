cb04349181658cfeb427782a6c11d8b8c231ba38
Jens Låås <jens@laas.mine.nu>
2015-03-06 14:25:27 +0100

    alt source location

--- all/nano-2.2.4-1/Fetch-source.sh
+++ all/nano-2.2.4-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=nano-2.2.4.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://www.nano-editor.org/dist/v2.2/$SRC
+[ -s "$DST" ] || wget -O $DST http://www.nano-editor.org/dist/v2.2/$SRC || wget -O $DST ftp://ftp.gnu.org/pub/gnu/nano/$SRC
