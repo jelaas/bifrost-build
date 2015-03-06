f912bc35b508597ad72fc7d0c9fd2c14c98377b6
Jens Låås <jens@laas.mine.nu>
2015-03-06 14:29:57 +0100

    adjust source location

--- all/e2fsprogs-1.41.11-1/Fetch-source.sh
+++ all/e2fsprogs-1.41.11-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=e2fsprogs-1.41.11.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://surfnet.dl.sourceforge.net/project/e2fsprogs/e2fsprogs/1.41.11/$SRC
+[ -s "$DST" ] || wget -O $DST http://downloads.sourceforge.net/project/e2fsprogs/e2fsprogs/1.41.11/$SRC
