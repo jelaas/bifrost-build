c64aa02507ee6fec8568e6c4564e1fe87b7449b4
Javier Lopez <m@javier.io>
2015-03-09 13:00:43 -0600

    sync new recipes with upstream

---
+++ all/pciutils-3.2.1-2/Build.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+SRCVER=pciutils-3.2.1
+PKG=$SRCVER-2 # with build version
+
+# PKGDIR is set by 'pkg_build'. Usually "/var/lib/build/all/$PKG".
+PKGDIR=${PKGDIR:-/var/lib/build/all/$PKG}
+SRC=/var/spool/src/$SRCVER.tar.gz
+BUILDDIR=/var/tmp/src/$SRCVER
+DST="/var/tmp/install/$PKG"
+
+#########
+# Simple inplace edit with sed.
+# Usage: sedit 's/find/replace/g' Filename
+function sedit {
+    sed "$1" $2 > /tmp/sedit.$$
+    cp /tmp/sedit.$$ $2
+    rm /tmp/sedit.$$
+}
+
+#########
+# Fetch sources
+./Fetch-source.sh || exit 1
+pkg_uninstall # Uninstall any dependencies used by Fetch-source.sh
+
+#########
+# Install dependencies:
+# pkg_available dependency1-1 dependency2-1
+pkg_install zlib-1.2.7-1 || exit 2
+
+#########
+# Unpack sources into dir under /var/tmp/src
+cd $(dirname $BUILDDIR); tar xf $SRC || exit 1
+
+#########
+# Patch
+cd $BUILDDIR || exit 1
+libtool_fix-1
+# patch -p1 < $PKGDIR/mypatch.pat
+
+#########
+# Configure
+
+#########
+# Post configure patch
+# patch -p0 < $PKGDIR/Makefile.pat
+
+#########
+# Compile
+make ZLIB=yes DNS=no PREFIX="/usr" SBINDIR=/sbin SHARED=no OPT="-Os -march=i586" LDFLAGS="-static" || exit 1
+
+#########
+# Install into dir under /var/tmp/install
+rm -rf "$DST"
+make install DESTDIR=$DST PREFIX="/usr" SBINDIR=/sbin SHARED=no
+mkdir -p $DST/usr/lib || exit 1
+cp $PKGDIR/pci.ids.gz $DST/usr/lib/pci.ids.gz || exit 1
+
+#########
+# Check result
+cd $DST
+# [ -f usr/bin/myprog ] || exit 1
+# (ldd sbin/myprog|grep -qs "not a dynamic executable") || exit 1
+
+#########
+# Clean up
+cd $DST
+rm -rf usr/share
+strip sbin/*
+
+#########
+# Make package
+cd $DST
+tar czf /var/spool/pkg/$PKG.tar.gz .
+
+#########
+# Cleanup after a success
+cd /var/lib/build
+[ "$DEVEL" ] || rm -rf "$DST"
+[ "$DEVEL" ] || rm -rf "$BUILDDIR"
+pkg_uninstall
+exit 0
