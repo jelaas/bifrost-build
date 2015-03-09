b69a8707ecd940a3e43c629b271961bf6abd0fe8
Javier Lopez <m@javier.io>
2015-03-09 00:01:10 -0600

    move broken recipes to broken/

--- broken/bandwidth-0.23a-1/Fetch-source.sh
+++ broken/bandwidth-0.23a-1/Fetch-source.sh
@@ -2,7 +2,7 @@
 
 SRC=bandwidth-0.23a.tar.gz
 DST=/var/spool/src/$SRC
-MD5=f0f78081b5ba66ea9e5eb4721dad3818
+MD5=
 
-[ -s "$DST" ] || wget -O $DST http://home.comcast.net/~fbui/$SRC \
+[ -s "$DST" ] || wget -O $DST http://example.com/$SRC \
  || ../../wget-finder -O $DST $SRC:$MD5
