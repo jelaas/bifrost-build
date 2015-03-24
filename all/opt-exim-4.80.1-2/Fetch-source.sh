277723524e7aed8d767d8c43306422d32c7b47f8
Jens Låås <jens@jelaas.eu>
2015-03-24 18:45:24 +0100

    opt-exim openssl update

---
+++ all/opt-exim-4.80.1-2/Fetch-source.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SRC=exim-4.80.1.tar.bz2
+DST=/var/spool/src/$SRC
+
+[ -s "$DST" ] || wget -O $DST ftp://ftp.exim.org/pub/exim/exim4/$SRC || wget -O $DST ftp://ftp.exim.org/pub/exim/exim4/old/$SRC
