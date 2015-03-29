c6239152164adf9a64f50e784115d93eb9e85ff6
Javier Lopez <m@javier.io>
2015-03-29 11:53:57 +0300

    keep quiet env-* scripts when stdin, stderr, stdout files already exists in chroot

--- util/env-setup.sh
+++ util/env-setup.sh
@@ -5,9 +5,9 @@
 [ -c /dev/urandom ] || mdev -s
 
 [ -d /proc/self/fd ] && [ ! -e /dev/fd ] && ln -s /proc/self/fd /dev/fd
-[ -L /dev/fd/0 ] && [ ! -e /dev/stdin ] && ln -s /dev/fd/0 /dev/stdin
-[ -L /dev/fd/1 ] && [ ! -e /dev/stdout ] && ln -s /dev/fd/1 /dev/stdout
-[ -L /dev/fd/2 ] && [ ! -e /dev/stderr ] && ln -s /dev/fd/2 /dev/stderr
+[ -L /dev/fd/0 ] && [ ! -e /dev/stdin ]  && ln -s /dev/fd/0 /dev/stdin  2>/dev/null
+[ -L /dev/fd/1 ] && [ ! -e /dev/stdout ] && ln -s /dev/fd/1 /dev/stdout 2>/dev/null
+[ -L /dev/fd/2 ] && [ ! -e /dev/stderr ] && ln -s /dev/fd/2 /dev/stderr 2>/dev/null
 
 [ -f /bin/env-hooks.sh ] && . /bin/env-hooks.sh
 
