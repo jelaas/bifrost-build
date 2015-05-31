e07140e610ba3a1b11f7def586368d6c97208f6b
Javier Lopez <m@javier.io>
2015-05-31 04:38:20 +0300

    fix typo

--- all/dnsmasq-2.71-2/Fetch-source.sh
+++ all/dnsmasq-2.71-2/Fetch-source.sh
@@ -4,5 +4,5 @@ SRC=dnsmasq-2.71.tar.gz
 DST=/var/spool/src/$SRC
 MD5=15a68f7f6cc0119e843f67d2f79598f1
 
-[ -s "$DST" ] || wget -O $DST http://www.thekelleys.org.uk/dnsmasq/$SRC
+[ -s "$DST" ] || wget -O $DST http://www.thekelleys.org.uk/dnsmasq/$SRC \
  || ../../wget-finder -O $DST $SRC:$MD5
