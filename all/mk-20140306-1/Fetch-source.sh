ca62465abaa4168f8cff92d984d9f1f8eca4e800
Javier Lopez <m@javier.io>
2015-06-30 22:09:57 +0300

    add fixed plan9port url, I hate openbsd mirrors

--- all/mk-20140306-1/Fetch-source.sh
+++ all/mk-20140306-1/Fetch-source.sh
@@ -6,5 +6,7 @@ SHA512=a0671163940186fd3d8cf37f06824f0d5a4b9734c1e70eb3174cd7f6b9a1ae769cf78e067
 
 if [ ! -s "$DST" ]; then
     pkg_install openssl-0.9.8n-2 || exit 2
+    #its disturbing the amount of corrupted plan9port tars the Openbsd mirrored
+    wget -O $DST http://ftp.cc.uoc.gr/mirrors/OpenBSD/distfiles/plan9port/$SRC || \
     ../../wget-finder -O $DST $SRC:$SHA512 || exit 1
 fi
