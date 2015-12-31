0e491761f92e01f0cbad758b672b4d37008f434d
Javier Lopez <m@javier.io>
2015-12-31 05:54:57 +0300

    add additional source url

--- all/opt-avrdude-5.10-1/Fetch-source.sh
+++ all/opt-avrdude-5.10-1/Fetch-source.sh
@@ -5,4 +5,5 @@ DST=/var/spool/src/$SRC
 MD5=69b082683047e054348088fd63bad2ff
 
 [ -s "$DST" ] || wget -O $DST http://nongnu.askapache.com/avrdude/$SRC \
+              || wget -O $DST http://download.savannah.gnu.org/releases/avrdude/$SRC \
  || ../../wget-finder -O $DST $SRC:$MD5
