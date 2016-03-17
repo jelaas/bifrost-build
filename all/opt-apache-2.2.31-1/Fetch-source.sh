0e9e054ecaf898943ef9bd7ad9500a8fce020b96
Jens Låås <jens@jelaas.eu>
2016-03-17 12:52:33 +0100

    new apache with latest openssl

---
+++ all/opt-apache-2.2.31-1/Fetch-source.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SRC=httpd-2.2.31.tar.bz2
+DST=/var/spool/src/$SRC
+
+pkg_install wget-1.12-2 || exit 2
+
+[ -s "$DST" ] || wget -O $DST http://apache.archive.sunet.se/dist/httpd/$SRC || wget --no-check-certificate -O $DST https://archive.apache.org/dist/httpd/$SRC
