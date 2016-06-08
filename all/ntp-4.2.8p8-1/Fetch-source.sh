42e103037d681642a016db92faaa8a50a18b5bf6
Jens Låås <jens@jelaas.eu>
2016-06-08 10:37:23 +0200

    security update

---
+++ all/ntp-4.2.8p8-1/Fetch-source.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SRC=ntp-4.2.8p8.tar.gz
+DST=/var/spool/src/$SRC
+
+pkg_install wget-1.15-3 || exit 2
+[ -s "$DST" ] || wget --no-check-certificate -O $DST http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-4.2/$SRC
