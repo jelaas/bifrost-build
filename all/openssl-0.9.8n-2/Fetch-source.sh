170cfe244bacafc2a06e59b9c0419522bde77729
Jens Låås <jens@jelaas.eu>
2016-06-09 19:57:05 +0200

    use curl to fetch

--- all/openssl-0.9.8n-2/Fetch-source.sh
+++ all/openssl-0.9.8n-2/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=openssl-0.9.8n.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://www.openssl.org/source/$SRC
+[ -s "$DST" ] || wget -O $DST http://www.openssl.org/source/$SRC || curl -L -k -o $DST http://www.openssl.org/source/$SRC
