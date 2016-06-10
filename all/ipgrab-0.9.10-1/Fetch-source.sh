a38377705da335f629dd2d70c6d5d0660584d048
Jens Låås <jens@jelaas.eu>
2016-06-10 16:13:10 +0200

    correct source url

--- all/ipgrab-0.9.10-1/Fetch-source.sh
+++ all/ipgrab-0.9.10-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=ipgrab-0.9.10.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://sunet.dl.sourceforge.net/project/ipgrab/ipgrab/0.9.10/$SRC
+[ -s "$DST" ] || wget -O $DST http://downloads.sourceforge.net/project/ipgrab/ipgrab/0.9.10/$SRC
