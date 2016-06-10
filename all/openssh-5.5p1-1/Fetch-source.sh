7451f6760fe5a806c13e65f3b27c9517c866ce29
Jens Låås <jens@jelaas.eu>
2016-06-10 08:52:42 +0200

    source location

--- all/openssh-5.5p1-1/Fetch-source.sh
+++ all/openssh-5.5p1-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=openssh-5.5p1.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST ftp://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/$SRC
+[ -s "$DST" ] || wget -O $DST ftp://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/$SRC || wget -O $DST http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/$SRC
