a6f27028c380026ba79d1629f4205e1d9d8d3cc3
Jens Låås <jens@jelaas.eu>
2016-06-10 16:40:53 +0200

    change source location

--- all/telnet-bsd-1.2-1/Fetch-source.sh
+++ all/telnet-bsd-1.2-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=telnet-bsd-1.2.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://sunsite.icm.edu.pl/pub/Linux/distributions/suse/people/kukuk/ipv6/$SRC || wget -O $DST ftp://ftp.suse.com/pub/people/kukuk/ipv6/$SRC
+[ -s "$DST" ] || wget -O $DST ftp://ftp.fi.debian.org/gentoo/distfiles/$SRC
