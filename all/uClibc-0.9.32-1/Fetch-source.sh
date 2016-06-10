053d1ae2814d24d33cc20d5633847fe62f65bf1a
Jens Låås <jens@jelaas.eu>
2016-06-10 16:25:06 +0200

    use curl to fetch source

--- all/uClibc-0.9.32-1/Fetch-source.sh
+++ all/uClibc-0.9.32-1/Fetch-source.sh
@@ -3,4 +3,5 @@
 SRC=uClibc-0.9.32.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://uclibc.org/downloads/$SRC
+pkg_install curl-7.49.1-1 || exit 2
+[ -s "$DST" ] || curl -L -k -o $DST http://uclibc.org/downloads/$SRC
