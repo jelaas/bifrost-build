14cfe44dc72926f566fa8735120d8179f4b260df
Jens Låås <jens@jelaas.eu>
2016-06-10 13:18:39 +0200

    remove bad source location

--- all/net-tools-1.60-2/Fetch-source.sh
+++ all/net-tools-1.60-2/Fetch-source.sh
@@ -3,6 +3,5 @@
 SRC=net-tools-1.60.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://www.tazenda.demon.co.uk/phil/net-tools/$SRC \
-              || wget -O $DST http://mirrors.slackware.com/slackware/slackware-13.37/source/n/net-tools/$SRC \
+[ -s "$DST" ] || wget -O $DST http://mirrors.slackware.com/slackware/slackware-13.37/source/n/net-tools/$SRC \
               || wget -O $DST http://pkgs.fedoraproject.org/repo/pkgs/net-tools/net-tools-1.60.tar.bz2/888774accab40217dde927e21979c165/$SRC
