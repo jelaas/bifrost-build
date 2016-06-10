ecce50c1cbd0470b4f51f75f50b4a03c6657699b
Jens Låås <jens@jelaas.eu>
2016-06-10 14:50:23 +0200

    correct source location

--- all/traceroute-2.0.16-1/Fetch-source.sh
+++ all/traceroute-2.0.16-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=traceroute-2.0.16.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://downloads.sourceforge.net/project/traceroute/traceroute/traceroute-2.0.16/$SRC || wget -O $DST http://sunsite.icm.edu.pl/pub/Linux/distributions/bifrost/download/src/$SRC
+[ -s "$DST" ] || wget -O $DST http://pkgs.fedoraproject.org/repo/pkgs/traceroute/traceroute-2.0.16.tar.gz/3e84c525cdb4184d64aacaf9225e6503/$SRC
