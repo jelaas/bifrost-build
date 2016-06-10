fb56a4718096920637f1b87a51de339b070458c1
Jens Låås <jens@jelaas.eu>
2016-06-10 16:43:10 +0200

    use curl to fetch source

--- all/xz-5.0.3-1/Fetch-source.sh
+++ all/xz-5.0.3-1/Fetch-source.sh
@@ -3,4 +3,5 @@
 SRC=xz-5.0.3.tar.bz2 
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://tukaani.org/xz/$SRC
+pkg_install curl-7.49.1-1 || exit 2
+[ -s "$DST" ] || curl -L -k -o $DST http://tukaani.org/xz/$SRC
