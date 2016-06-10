a3f9ed788d7becbf805da54cdbcce8c0089701c3
Jens Låås <jens@jelaas.eu>
2016-06-10 15:05:57 +0200

    correct source location

--- all/whois_5.0.10-2/Fetch-source.sh
+++ all/whois_5.0.10-2/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=whois_5.0.10.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://ftp.debian.org/debian/pool/main/w/whois/$SRC
+[ -s "$DST" ] || wget -O $DST http://archive.debian.org/debian/pool/main/w/whois/$SRC
