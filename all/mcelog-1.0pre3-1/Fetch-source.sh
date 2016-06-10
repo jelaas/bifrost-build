2d275422c63dcf0cc0c1108e096927c50df8a45c
Jens Låås <jens@jelaas.eu>
2016-06-10 15:49:23 +0200

    fetch mcelog from git

--- all/mcelog-1.0pre3-1/Fetch-source.sh
+++ all/mcelog-1.0pre3-1/Fetch-source.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-SRC=mcelog-1.0pre3.tar.bz2
+VER=mcelog-1.0pre3
+SRC=$VER.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://www.kernel.org/pub/linux/utils/cpu/mce/$SRC || wget -O $DST http://sunsite.icm.edu.pl/pub/Linux/distributions/bifrost/download/src/$SRC
+if [ ! -s "$DST" ]; then
+    pkg_install passwd-file-1 || exit 2
+    pkg_install git-1.7.1-2 || exit 2
+    pkg_install openssh-5.5p1-1 || exit 2
+    cd /tmp
+    rm -rf $VER
+    /opt/git/bin/git clone git://git.kernel.org/pub/scm/utils/cpu/mce/mcelog.git $VER || exit 1
+    cd $VER  || exit 1
+    /opt/git/bin/git checkout RELEASE_1_0_PRE3 || exit 1
+    cd /tmp
+    tar czf $DST $VER
+    rm -rf $VER
+fi
