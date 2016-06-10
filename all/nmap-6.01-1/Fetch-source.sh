fe3799381db78b4b4501c5d674da1b8e456a8a2d
Jens Låås <jens@jelaas.eu>
2016-06-10 16:18:58 +0200

    use curl to fetch source

--- all/nmap-6.01-1/Fetch-source.sh
+++ all/nmap-6.01-1/Fetch-source.sh
@@ -3,4 +3,5 @@
 SRC=nmap-6.01.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST  http://nmap.org/dist/$SRC
+pkg_install curl-7.49.1-1 || exit 2
+[ -s "$DST" ] || curl -L -k -o $DST  http://nmap.org/dist/$SRC
