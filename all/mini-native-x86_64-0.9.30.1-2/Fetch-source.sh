7a6093a502f0a56f59c67fd157459b6ea80b8c2d
Jens Låås <jens@jelaas.eu>
2016-06-10 09:06:58 +0200

    use curl to fetch

--- all/mini-native-x86_64-0.9.30.1-2/Fetch-source.sh
+++ all/mini-native-x86_64-0.9.30.1-2/Fetch-source.sh
@@ -3,4 +3,5 @@
 SRC=mini-native-x86_64-0.9.30.1.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://uclibc.org/downloads/binaries/0.9.30.1/mini-native-x86_64.tar.bz2
+pkg_install curl-7.49.1-1 || exit 2
+[ -s "$DST" ] || wget -O $DST http://uclibc.org/downloads/binaries/0.9.30.1/mini-native-x86_64.tar.bz2 || curl -L -k -o $DST http://uclibc.org/downloads/binaries/0.9.30.1/mini-native-x86_64.tar.bz2
