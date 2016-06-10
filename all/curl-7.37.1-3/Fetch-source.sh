6e4b079c1f02954d9fa39f7cad1dc31a98aa0fa8
Jens Låås <jens@jelaas.eu>
2016-06-10 16:10:41 +0200

    use curl to fetch source

--- all/curl-7.37.1-3/Fetch-source.sh
+++ all/curl-7.37.1-3/Fetch-source.sh
@@ -3,4 +3,5 @@
 SRC=curl-7.37.1.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://curl.haxx.se/download/$SRC
+pkg_install curl-7.49.1-1 || exit 2
+[ -s "$DST" ] || wget -O $DST http://curl.haxx.se/download/$SRC || curl -L -k -o $DST http://curl.haxx.se/download/$SRC
