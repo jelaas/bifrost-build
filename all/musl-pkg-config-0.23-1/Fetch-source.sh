d1e86565c4ceba280f3121f764844497f6e185d7
Jens Låås <jens@jelaas.eu>
2016-06-10 12:46:05 +0200

    use curl to fetch source

--- all/musl-pkg-config-0.23-1/Fetch-source.sh
+++ all/musl-pkg-config-0.23-1/Fetch-source.sh
@@ -3,4 +3,5 @@
 SRC=pkg-config-0.23.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://pkg-config.freedesktop.org/releases/$SRC
+pkg_install curl-7.49.1-1 || exit 2
+[ -s "$DST" ] || wget -O $DST http://pkg-config.freedesktop.org/releases/$SRC || curl -L -k -o $DST http://pkg-config.freedesktop.org/releases/$SRC
