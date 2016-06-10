ceadfd8640d2cfa7b353d11f336e37ae389a29e0
Jens Låås <jens@jelaas.eu>
2016-06-10 13:14:15 +0200

    fix source url

--- all/netkit-base-0.17-1/Fetch-source.sh
+++ all/netkit-base-0.17-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=netkit-base-0.17.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://sunsite.icm.edu.pl/pub/Linux/distributions/rootlinux/ports/base/netkit/netkit-base-0.17.tar.gz || wget -O $DST ftp://ftp.linux.org.uk/pub/linux/Networking/netkit/$SRC
+[ -s "$DST" ] || wget -O $DST http://sunsite.icm.edu.pl/pub/Linux/distributions/rootlinux/ports/base/netkit/netkit-base-0.17.tar.gz || wget -O $DST http://ftp.linux.org.uk/pub/linux/Networking/netkit/$SRC
