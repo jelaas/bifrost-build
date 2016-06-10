99271e81e30df2168ded98914804d0189dcd2b42
Jens Låås <jens@jelaas.eu>
2016-06-10 08:56:18 +0200

    source location

--- all/flex-2.5.35-1/Fetch-source.sh
+++ all/flex-2.5.35-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=flex-2.5.35.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://switch.dl.sourceforge.net/project/flex/flex/flex-2.5.35/$SRC
+[ -s "$DST" ] || wget -O $DST http://downloads.sourceforge.net/project/flex/flex/flex-2.5.35/$SRC
