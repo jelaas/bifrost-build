9309d5e2eae76609f667e6cca8ee4e447c2480eb
Jens Låås <jens@jelaas.eu>
2016-06-11 14:01:54 +0200

    change source location

--- all/vlock-1.4-1/Fetch-source.sh
+++ all/vlock-1.4-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=vlock-1.4.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://cthulhu.c3d2.de/~toidinamai/vlock/archive/$SRC || wget -O $DST http://sunsite.icm.edu.pl/pub/os/Linux/distributions/bifrost/download/src/$SRC
+[ -s "$DST" ] || wget -O $DST http://www.sourcefiles.org/System/Administration/Miscellaneous/$SRC
