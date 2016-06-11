750357e5debc59961c4d993791e437b18988fc37
Jens Låås <jens@jelaas.eu>
2016-06-11 13:32:15 +0200

    change source location

--- all/logrotate-3.7.3-1/Fetch-source.sh
+++ all/logrotate-3.7.3-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=logrotate-3.7.3.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://ibiblio.download.sourcemage.org/pub/linux/distributions/sourcemage/mirror/$SRC || wget -O $DST http://dbg.download.sourcemage.org/mirror/$SRC
+[ -s "$DST" ] || wget -O $DST http://pkgs.fedoraproject.org/repo/pkgs/logrotate/logrotate-3.7.3.tar.gz/266c48560ba588649f815a0c59edb488/$SRC
