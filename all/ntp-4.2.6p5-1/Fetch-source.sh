460ad1040e3b51720c757e35069a9181821f7849
Jens Låås <jens@jelaas.eu>
2016-06-11 14:43:16 +0200

    change source location

--- all/ntp-4.2.6p5-1/Fetch-source.sh
+++ all/ntp-4.2.6p5-1/Fetch-source.sh
@@ -3,4 +3,5 @@
 SRC=ntp-4.2.6p5.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-4.2/$SRC
+pkg_install wget-1.15-3 || exit 2
+[ -s "$DST" ] || wget --no-check-certificate -O $DST http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-4.2/$SRC
