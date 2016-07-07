90c722c4206f81806b9af55a46f3ffbc52e39ddc
Jens Låås <jens.laas@uadm.uu.se>
2016-07-07 14:04:41 +0200

    zlib built w musl 1.1.15

---
+++ all/musl-zlib-1.2.8-2/Fetch-source.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SRC=zlib-1.2.8.tar.gz
+DST=/var/spool/src/$SRC
+
+pkg_install tarmd-zlib-1.2-1 || exit 2
+
+[ -s "$DST" ] || tarmd 1b6db52e68ab5ab1326cb1909490148d4eec7d5fbd6fcff93a78c47a5c25a15a $DST \
+ /usr/bin/wget -O- http://zlib.net/$SRC || \
+ tarmd 1b6db52e68ab5ab1326cb1909490148d4eec7d5fbd6fcff93a78c47a5c25a15a $DST\
+ /usr/bin/wget -O- ftp://ftp.simplesystems.org/pub/libpng/png/src/history/$SRC
