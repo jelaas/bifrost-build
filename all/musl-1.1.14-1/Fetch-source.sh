e45fb9d7f739d6d401c566e6c4e9564e2f9da6d9
Jens Låås <jens.laas@uadm.uu.se>
2016-07-07 14:02:26 +0200

    check sha256 of source

--- all/musl-1.1.14-1/Fetch-source.sh
+++ all/musl-1.1.14-1/Fetch-source.sh
@@ -3,4 +3,6 @@
 SRC=musl-1.1.14.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://www.musl-libc.org/releases/$SRC
+pkg_install tarmd-zlib-1.2-1 || exit 2
+
+[ -s "$DST" ] || tarmd 2b45daa03169d684718b6b72381c2b786501540861303d5fb415fc40a6a749c8 $DST /usr/bin/wget -O- http://www.musl-libc.org/releases/$SRC
