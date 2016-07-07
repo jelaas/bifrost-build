8c84393f0c2ae8c8ac1f1f6a84700cad1d025e55
Jens Låås <jens.laas@uadm.uu.se>
2016-07-07 14:00:47 +0200

    check sha256 of source

--- all/musl-zlib-1.2.8-1/Fetch-source.sh
+++ all/musl-zlib-1.2.8-1/Fetch-source.sh
@@ -3,4 +3,9 @@
 SRC=zlib-1.2.8.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://www.zlib.net/$SRC || wget -O $DST ftp://ftp.simplesystems.org/pub/libpng/png/src/history/$SRC
+pkg_install tarmd-zlib-1.2-1 || exit 2
+
+[ -s "$DST" ] || tarmd 1b6db52e68ab5ab1326cb1909490148d4eec7d5fbd6fcff93a78c47a5c25a15a $DST \
+ /usr/bin/wget -O- http://zlib.net/$SRC || \
+ tarmd 1b6db52e68ab5ab1326cb1909490148d4eec7d5fbd6fcff93a78c47a5c25a15a $DST\
+ /usr/bin/wget -O- ftp://ftp.simplesystems.org/pub/libpng/png/src/history/$SRC
