df094383334a3c1972aac02f9eb248b8bf0ff4a4
Jens Låås <jens.laas@uadm.uu.se>
2016-07-19 09:18:32 +0200

    verify source

--- all/tar-1.23-1/Fetch-source.sh
+++ all/tar-1.23-1/Fetch-source.sh
@@ -2,5 +2,8 @@
 
 SRC=tar-1.23.tar.bz2
 DST=/var/spool/src/$SRC
+SHA=ddac082c9daad82a2e4d9c7d9038828c886fb8c341d91d1d886024c2e8baf457
 
-[ -s "$DST" ] || wget -O $DST ftp://sunsite.icm.edu.pl/pub/gnu/tar/$SRC
+pkg_install tarmd-1.2-1 || exit 2
+
+[ -s "$DST" ] || tarmd $SHA $DST /usr/bin/wget -O- ftp://sunsite.icm.edu.pl/pub/gnu/tar/$SRC
