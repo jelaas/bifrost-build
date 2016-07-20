64b6e605a506866c95a27a2a13281d049a778074
Jens Låås <jens@jelaas.eu>
2016-07-20 08:54:51 +0200

    source verification

--- all/ncurses-bin-5.7-1/Fetch-source.sh
+++ all/ncurses-bin-5.7-1/Fetch-source.sh
@@ -2,5 +2,7 @@
 
 SRC=ncurses-5.7.tar.gz
 DST=/var/spool/src/$SRC
+SHA=f9287ad5839bbd398f5f185d28b4e6041ced2758ed5b03a616bb3365e3dc5db8
 
-[ -s "$DST" ] || wget -O $DST ftp://sunsite.icm.edu.pl/pub/gnu/ncurses/$SRC
+pkg_install tarmd-1.2-1 || exit 2
+[ -s "$DST" ] || tarmd $SHA $DST wget -O- ftp://sunsite.icm.edu.pl/pub/gnu/ncurses/$SRC
