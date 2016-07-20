1459ec709662ebc1596f3ec7e1ebdaf6e2d1193d
Jens Låås <jens@jelaas.eu>
2016-07-20 08:47:19 +0200

    source verification

--- all/zile-2.3.14-2/Fetch-source.sh
+++ all/zile-2.3.14-2/Fetch-source.sh
@@ -2,5 +2,7 @@
 
 SRC=zile-2.3.14.tar.gz
 DST=/var/spool/src/$SRC
+SHA=98b7f3157669d7e5da20ce94828085866679977c587592409e9664298cf45de5
 
-[ -s "$DST" ] || wget -O $DST ftp://sunsite.icm.edu.pl/pub/gnu/zile/$SRC
+pkg_install tarmd-1.2-1 || exit 2
+[ -s "$DST" ] || tarmd $SHA $DST wget -O- ftp://sunsite.icm.edu.pl/pub/gnu/zile/$SRC
