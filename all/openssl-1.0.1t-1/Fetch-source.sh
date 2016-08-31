16c7826eea0c2fe791c90d21f8312f931b4dc089
Jens Låås <jens@jelaas.eu>
2016-08-31 14:33:25 +0200

    openssl update

---
+++ all/openssl-1.0.1t-1/Fetch-source.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SRC=openssl-1.0.1t.tar.gz
+DST=/var/spool/src/$SRC
+SHA=3640727bf98e70de068d3783a066b8684d620243bb45451f46a63e07d1f60432
+
+pkg_install tarmd-1.2-1 || exit 2
+pkg_install wget-1.15-3 || exit 2
+
+[ -s "$DST" ] || tarmd $SHA $DST wget --no-check-certificate -O- http://www.openssl.org/source/$SRC
