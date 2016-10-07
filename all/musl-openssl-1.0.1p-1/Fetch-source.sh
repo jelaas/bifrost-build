3eeca3e51a81aee2b102076fb5030fe06f916ce8
Jens Låås <jens@jelaas.eu>
2016-10-07 12:42:09 +0200

    fix source fetch

--- all/musl-openssl-1.0.1p-1/Fetch-source.sh
+++ all/musl-openssl-1.0.1p-1/Fetch-source.sh
@@ -2,7 +2,7 @@
 
 SRC=openssl-1.0.1p.tar.gz
 DST=/var/spool/src/$SRC
-SHA=ddc4ef7cdc9de8596199d692b61c26219c0198cf1f1f9188bbd81a98df315f5c
+SHA=4572bfdd47bd214a12232f0cadba9e83be14afaea5bf3b0abfe85970b4360255
 
 pkg_install tarmd-1.2-1 || exit 2
 pkg_install curl-7.49.1-1 || exit 2
