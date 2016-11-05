63b7cfbe59a57fd8e233ed63f4f54272219d62aa
Jens Låås <jens@jelaas.eu>
2016-11-05 15:40:32 +0100

    mtr and newer git

--- all/bifrost-7.2.2-1/Build.sh
+++ all/bifrost-7.2.2-1/Build.sh
@@ -110,7 +110,7 @@ ln -s /usr/bin/bzip2 ./usr/bin/bzcat
 untar diffutils-3.0-1
 untar file-5.04-1
 untar findutils-4.4.2-1
-untar git-1.8.2.1-3
+untar git-2.10.2-1
 untar grep-2.6.3-2
 untar gzip-1.4-1
 untar less-436-1
@@ -175,6 +175,7 @@ untar arpwatch-2.1a15-1
 untar conntrack-tools-0.9.14-2
 untar curl-7.51.0-1
 untar ipgrab-0.9.10-1
+untar mtr-0.86-1 ./usr/sbin/mtr
 untar ncftp-3.2.5-1 ./usr/bin/ncftp
 untar netperf-2.4.5-1
 untar nmap-6.01-1
