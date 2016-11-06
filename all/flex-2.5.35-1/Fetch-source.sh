05b4d0222e285eccce0ef8ae718be2928cae9dc1
Jens Låås <jens@jelaas.eu>
2016-11-06 14:09:16 +0100

    change source url

--- all/flex-2.5.35-1/Fetch-source.sh
+++ all/flex-2.5.35-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=flex-2.5.35.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://downloads.sourceforge.net/project/flex/flex/flex-2.5.35/$SRC
+[ -s "$DST" ] || wget -O $DST http://pkgs.fedoraproject.org/repo/pkgs/flex/flex-2.5.35.tar.bz2/10714e50cea54dc7a227e3eddcd44d57/$SRC
