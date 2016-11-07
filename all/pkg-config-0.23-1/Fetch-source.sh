25eba5f4074d33d7eda11a5059fa4a980873eb99
Jens Låås <jens@jelaas.eu>
2016-11-07 15:29:00 +0100

    fetch from https

--- all/pkg-config-0.23-1/Fetch-source.sh
+++ all/pkg-config-0.23-1/Fetch-source.sh
@@ -3,4 +3,6 @@
 SRC=pkg-config-0.23.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://pkg-config.freedesktop.org/releases/$SRC
+pkg_install wget-1.18-1 || exit 2
+
+[ -s "$DST" ] || wget --no-check-certificate -O $DST http://pkg-config.freedesktop.org/releases/$SRC
