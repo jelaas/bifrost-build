2878b52259491a3f754315d2baae98694fd1a7db
Jens Låås <jens@jelaas.eu>
2016-11-07 15:36:18 +0100

    fix source locagtion

--- all/usbutils-001-1/Fetch-source.sh
+++ all/usbutils-001-1/Fetch-source.sh
@@ -3,5 +3,4 @@
 SRC=usbutils-001.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://mirror.linux.org.au/linux/utils/usb/usbutils/$SRC || wget -O $DST ftp://ftp.kernel.org/pub/linux/utils/usb/usbutils/$SRC || wget -O $DST http://sunsite.icm.edu.pl/pub/Linux/kernel.org/linux/utils/usb/usbutils/$SRC
-
+[ -s "$DST" ] || wget -O $DST http://pkgs.fedoraproject.org/repo/pkgs/usbutils/usbutils-001.tar.gz/3ecdcb42f08ef0d63e2638feb06ececc/$SRC || wget -O $DST http://mirror.linux.org.au/linux/utils/usb/usbutils/$SRC || wget -O $DST http://sunsite.icm.edu.pl/pub/Linux/kernel.org/linux/utils/usb/usbutils/$SRC
