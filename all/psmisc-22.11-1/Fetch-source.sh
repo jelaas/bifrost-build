35a0bbcf712987045810d818c122cb225d57079b
Jens Låås <jens@jelaas.eu>
2016-11-08 11:39:09 +0100

    use wget with https support

--- all/psmisc-22.11-1/Fetch-source.sh
+++ all/psmisc-22.11-1/Fetch-source.sh
@@ -4,4 +4,4 @@ SRC=psmisc-22.11.tar.gz
 DST=/var/spool/src/$SRC
 
 pkg_install wget-1.18-1 || exit 2
-[ -s "$DST" ] || wget -O $DST http://downloads.sourceforge.net/project/psmisc/psmisc/Archive/$SRC || wget -O $DST http://downloads.sourceforge.net/project/psmisc/psmisc/$SRC
+[ -s "$DST" ] || wget --no-check-certificate -O $DST http://downloads.sourceforge.net/project/psmisc/psmisc/Archive/$SRC || wget --no-check-certificate -O $DST http://downloads.sourceforge.net/project/psmisc/psmisc/$SRC
