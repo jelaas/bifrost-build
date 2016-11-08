cce4f77e412eb4017a62b69e1698204100aed347
Jens Låås <jens@jelaas.eu>
2016-11-08 10:31:49 +0100

    typo

--- all/bifrost-7.2.2-1/Build.sh
+++ all/bifrost-7.2.2-1/Build.sh
@@ -319,7 +319,7 @@ untar make-3.81-1
 untar module-init-tools-3.15-1
 untar ncurses-bin-5.7-1 ./usr/bin/clear
 untar numactl-2.0.6-1 ./bin/numactl
-untar musl-openssl-1.1.0b-1/ ./usr/bin/openssl
+untar musl-openssl-1.1.0b-1 ./usr/bin/openssl
 untar procps-3.2.8-2 ./usr/bin/free
 untar procps-3.2.8-2 ./usr/bin/w
 untar procps-3.2.8-2 ./usr/bin/top
