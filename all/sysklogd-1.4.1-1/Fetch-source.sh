6ee4193c8533fea3d541fa27f831b771255f2bd6
Jens Låås <jens@jelaas.eu>
2016-11-08 12:02:43 +0100

    source location and verification

--- all/sysklogd-1.4.1-1/Fetch-source.sh
+++ all/sysklogd-1.4.1-1/Fetch-source.sh
@@ -2,5 +2,7 @@
 
 SRC=sysklogd-1.4.1.tar.gz
 DST=/var/spool/src/$SRC
+SHA=42c71b067fc6d780b171315fc28803bf82bed450c068e91e702b816f4fd929be
 
-[ -s "$DST" ] || wget -O $DST http://shlrm.org/sourcemage/$SRC
+pkg_install tarmd-1.2-1 || exit 2
+[ -s "$DST" ] || tarmd $SHA $DST wget -O- http://www.infodrom.org/projects/sysklogd/download/$SRC
