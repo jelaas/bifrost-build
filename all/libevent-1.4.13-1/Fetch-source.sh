4ea85655a13d188065103bfca18a9e22b1dac8a9
Jens Låås <jens@jelaas.eu>
2016-11-25 12:39:24 +0100

    fix source

--- all/libevent-1.4.13-1/Fetch-source.sh
+++ all/libevent-1.4.13-1/Fetch-source.sh
@@ -3,4 +3,6 @@
 SRC=libevent-1.4.13-stable.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://www.monkey.org/~provos/$SRC
+pkg_install wget-1.18-1 || exit 2
+
+[ -s "$DST" ] || wget --no-check-certificate -O $DST http://www.monkey.org/~provos/$SRC
--- all/nfs-utils-1.1.6-1/Fetch-source.sh
+++ all/nfs-utils-1.1.6-1/Fetch-source.sh
@@ -3,4 +3,6 @@
 SRC=nfs-utils-1.1.6.tar.bz2
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://freefr.dl.sourceforge.net/project/nfs/nfs-utils/1.1.6/$SRC
+pkg_install wget-1.18-1 || exit 2
+
+[ -s "$DST" ] || wget --no-check-certificate -O $DST http://downloads.sourceforge.net/project/nfs/nfs-utils/1.1.6/$SRC
