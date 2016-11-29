95896cec1a1aebaeba340d5ea4227154a167cda8
Jens Låås <jens@jelaas.eu>
2016-11-29 12:48:59 +0100

    curl update

--- staging/musl-example-1.0.0-1/Fetch-source.sh
+++ staging/musl-example-1.0.0-1/Fetch-source.sh
@@ -5,5 +5,5 @@ DST=/var/spool/src/$SRC
 SHA=deadbeef
 
 pkg_install tarmd-1.2-1 || exit 2
-pkg_install curl-7.49.1-1 || exit 2
+pkg_install curl-7.51.0-1 || exit 2
 [ -s "$DST" ] || tarmd $SHA $DST curl -L -k http://source.on.internet/$SRC
