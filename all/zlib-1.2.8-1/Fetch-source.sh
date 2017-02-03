c88db001167891dd64cfd2e657e40dfc4bbe0cd7
Jens Låås <jens@jelaas.eu>
2017-02-03 17:54:16 +0100

    fix source location

--- all/zlib-1.2.8-1/Fetch-source.sh
+++ all/zlib-1.2.8-1/Fetch-source.sh
@@ -4,8 +4,9 @@ SRC=zlib-1.2.8.tar.gz
 DST=/tmp/$SRC
 VDST=/var/spool/src/$SRC
 
+pkg_install curl-7.51.0-1 || exit 2
 if ! [ -s "$VDST" ]; then
-	pkg_install tarmd-nocomp-1.2-1 || exit 1
-	wget -O $DST http://zlib.net/$SRC || wget -O $DST ftp://ftp.simplesystems.org/pub/libpng/png/src/history/zlib/$SRC
-	zcat "$DST" | tarmd 1b6db52e68ab5ab1326cb1909490148d4eec7d5fbd6fcff93a78c47a5c25a15a $VDST
+    pkg_install tarmd-nocomp-1.2-1 || exit 1
+    curl -L -k -o $DST https://downloads.sourceforge.net/project/libpng/zlib/1.2.8/$SRC || exit 1
+    zcat "$DST" | tarmd 1b6db52e68ab5ab1326cb1909490148d4eec7d5fbd6fcff93a78c47a5c25a15a $VDST
 fi
