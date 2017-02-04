c28c7da7d90583f03a35e1c7a3462bf9487389fd
Jens Låås <jens@jelaas.eu>
2017-02-04 13:04:13 +0100

    fix source location

--- all/musl-xz-5.2.2-1/Fetch-source.sh
+++ all/musl-xz-5.2.2-1/Fetch-source.sh
@@ -4,5 +4,5 @@ SRC=xz-5.2.2.tar.gz
 DST=/var/spool/src/$SRC
 
 pkg_install tarmd-zlib-1.2-1 || exit 2
-
-[ -s "$DST" ] || tarmd bc98a4257c19bffe7df9ad3101b72d1ade9df10418adf0a4139fb151cb3a0203 $DST /usr/bin/wget -O- http://tukaani.org/xz/$SRC
+pkg_install curl-7.51.0-1 || exit 2
+[ -s "$DST" ] || tarmd bc98a4257c19bffe7df9ad3101b72d1ade9df10418adf0a4139fb151cb3a0203 $DST curl -L -k http://tukaani.org/xz/$SRC
