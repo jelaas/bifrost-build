65973a5651c625f718f7babd2c133c93c1e6578c
Jens Låås <jens@jelaas.eu>
2017-02-05 14:16:18 +0100

    looking for bug

--- all/meta-bifrost-bin64-7/Build.sh
+++ all/meta-bifrost-bin64-7/Build.sh
@@ -47,20 +47,21 @@ setarch linux64 /bin/chroot /home/build/mini-native-x86_64 mount sysfs -t sysfs 
 
 echo Building 
 for F in ethtool-v3.16-1 strace-4.5.20-2 tcpdump-4.1.1-1 binutils-x86_64-2.20.1-1; do
+    echo BUILD: Building $F
     setarch linux64 /bin/chroot /home/build/mini-native-x86_64 /var/lib/build/pkg_build $F || exit 1
 done
 
-echo Unmounting proc and sysfs
-setarch linux64 /bin/chroot /home/build/mini-native-x86_64 umount /proc
-setarch linux64 /bin/chroot /home/build/mini-native-x86_64 umount /sys
+echo BUILD: packages built
 
 #########
 # Copy packages
 function untar {
     if [ ! -f /home/build/mini-native-x86_64/var/spool/pkg/$1.tar.gz ]; then
+	echo "BUILD ERROR: $1 not built. Trying to build it."
 	setarch linux64 /bin/chroot /home/build/mini-native-x86_64 /var/lib/build/pkg_build $1 || exit 1
 	exit 2
     fi
+    echo "BUILD UNTAR: $1"
     tar xvf /home/build/mini-native-x86_64/var/spool/pkg/$1.tar.gz $2 || exit 1
 }
 
@@ -68,10 +69,14 @@ rm -rf "$DST"
 mkdir -p $DST
 cd $DST || exit 1
 
-untar ethtool-v3.16-1
-untar strace-4.5.20-2
-untar tcpdump-4.1.1-1
-untar binutils-x86_64-2.20.1-1
+untar ethtool-v3.16-1 || exit 1
+untar strace-4.5.20-2 || exit 1
+untar tcpdump-4.1.1-1 || exit 1
+untar binutils-x86_64-2.20.1-1 || exit 1
+
+echo BUILD: Unmounting proc and sysfs
+setarch linux64 /bin/chroot /home/build/mini-native-x86_64 umount /proc
+setarch linux64 /bin/chroot /home/build/mini-native-x86_64 umount /sys
 
 cd $DST
 tar czf /var/spool/pkg/$PKG.tar.gz .
