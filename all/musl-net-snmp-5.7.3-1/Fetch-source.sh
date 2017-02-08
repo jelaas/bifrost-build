4c9348360b196d3f9c1641f291d61b207a83aef9
Jens Låås <jens@jelaas.eu>
2017-02-08 15:43:22 +0100

    source lcoation

--- all/musl-net-snmp-5.7.3-1/Fetch-source.sh
+++ all/musl-net-snmp-5.7.3-1/Fetch-source.sh
@@ -3,4 +3,5 @@
 SRC=net-snmp-5.7.3.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://downloads.sourceforge.net/project/net-snmp/net-snmp/5.7.3/$SRC
+pkg_install curl-7.51.0-1 || exit 2
+[ -s "$DST" ] || curl -L -k -o $DST http://downloads.sourceforge.net/project/net-snmp/net-snmp/5.7.3/$SRC
