e7137d7ba4181005929156333fe5e851ea5b2347
Jens Låås <jens@jelaas.eu>
2017-02-08 15:41:49 +0100

    source lcoation

--- all/musl-libnl-1.1-1/Fetch-source.sh
+++ all/musl-libnl-1.1-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=libnl-1.1.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://people.suug.ch/~tgr/libnl/files/$SRC
+[ -s "$DST" ] || wget -O $DST http://www.infradead.org/~tgr/libnl/files/$SRC
