395df9d71d753fb03dc3d8b673e95c4910f65be0
Javier Lopez <m@javier.io>
2017-03-17 21:36:48 -0400

    add additional mirrors for util-linux-bin-2.22.2-1

--- all/util-linux-bin-2.22.2-1/Fetch-source.sh
+++ all/util-linux-bin-2.22.2-1/Fetch-source.sh
@@ -5,4 +5,7 @@ DST=/var/spool/src/$SRC
 MD5=3e379b4d8b9693948d751c154614c73e
 
 [ -s "$DST" ] || wget -O $DST ftp://ftp.kernel.org/pub/linux/utils/util-linux/v2.22/$SRC \
+              || wget -O $DST http://ftp.iij.ad.jp/pub/linux/kernel/linux/utils/util-linux/v2.22/$SRC \
+              || wget -O $DST http://ftp.be.debian.org/pub/linux/utils/util-linux/v2.22/$SRC \
+              || wget -O $DST http://ubuntu.mirrorservice.org/sites/ftp.kernel.org/pub/linux/utils/util-linux/v2.22/$SRC \
  || ../../wget-finder -O $DST $SRC:$MD5
