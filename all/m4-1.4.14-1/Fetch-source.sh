22b4efe11ea4dd69f36aad403ca8597ba43fff0c
Javier Lopez <m@javier.io>
2017-03-17 21:47:42 -0400

    add additional mirrors for m4-1.4.14-1 && zlib-1.2.7-1

--- all/m4-1.4.14-1/Fetch-source.sh
+++ all/m4-1.4.14-1/Fetch-source.sh
@@ -5,4 +5,7 @@ DST=/var/spool/src/$SRC
 MD5=f0542d58f94c7d0ce0d01224e447be66
 
 [ -s "$DST" ] || wget -O $DST ftp://ftp.sunet.se/pub/gnu/m4/$SRC \
+              || wget -O $DST http://www-uxsup.csx.cam.ac.uk/pub/gnu/m4/$SRC \
+              || wget -O $DST http://ftp.acc.umu.se/mirror/gnu.org/gnu/m4/$SRC \
+              || wget -O $DST http://ftp.riken.jp/GNU/gnu/m4/$SRC \
  || ../../wget-finder -O $DST $SRC:$MD5
--- all/zlib-1.2.7-1/Fetch-source.sh
+++ all/zlib-1.2.7-1/Fetch-source.sh
@@ -5,5 +5,6 @@ DST=/var/spool/src/$SRC
 MD5=2ab442d169156f34c379c968f3f482dd
 
 [ -s "$DST" ] || wget -O $DST http://www.zlib.net/$SRC \
-              || wget -O $DST ftp://ftp.simplesystems.org/pub/libpng/png/src/history/zlib/$SRC \
+              || wget -O $DST http://pkgs.fedoraproject.org/repo/pkgs/zlib/zlib-1.2.7.tar.bz2/2ab442d169156f34c379c968f3f482dd/$SRC \
+              || wget -O $DST http://ftp.lfs-matrix.net/pub/clfs/conglomeration/zlib/$SRC \
  || ../../wget-finder -O $DST $SRC:$MD5
